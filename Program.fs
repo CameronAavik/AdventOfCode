700dda061f7da8f2242b44e7b1e02928193d5d35
Cameron Aavik <cameronaavik@gmail.com>
2017-12-04 20:30:31 +1000

    Actually finished Day 1

--- Program.fs
+++ Program.fs
@@ -1,15 +1,20 @@
 ﻿open System
 
 module Day1 =
-    let solve inputStr =
-        Seq.append inputStr [ Seq.head inputStr ] 
-        |> Seq.pairwise 
-        |> Seq.filter (fun (a, b) -> a = b) 
-        |> Seq.sumBy (fun (a, b) -> int a - int '0')
+    let solve inputStr windowSize = 
+        Seq.append inputStr inputStr
+        |> Seq.windowed windowSize
+        |> Seq.take (Seq.length inputStr)
+        |> Seq.filter (fun w -> Seq.head w = Seq.last w)
+        |> Seq.sumBy (fun w -> int (Seq.head w) - int '0')
+
+    let solvePart1 inputStr = solve inputStr 2
+    let solvePart2 inputStr = solve inputStr (((Seq.length inputStr) / 2) + 1)
 
 let runSolver (argv : string array) = 
     match argv.[0] with
-     | "Day1" -> printfn "%i" (Day1.solve argv.[1])
+     | "Day1.1" -> printfn "%i" (Day1.solvePart1 argv.[1])
+     | "Day1.2" -> printfn "%i" (Day1.solvePart2 argv.[1])
      | _ -> ()
 
 [<EntryPoint>]
