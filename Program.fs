45398ef40b832547410c311ea603fdb05e47cc7f
Cameron Aavik <cameronaavik@gmail.com>
2017-12-08 21:14:20 +1000

    Day 6 + dedicated place for input files

--- Program.fs
+++ Program.fs
@@ -11,7 +11,7 @@ module Day1 =
         |> Seq.windowed windowSize
         |> Seq.take (Seq.length captcha)
         |> Seq.filter (fun w -> Seq.head w = Seq.last w)
-        |> Seq.sumBy (Seq.head >> int >> (-) (int '0'))
+        |> Seq.sumBy (Seq.head >> int >> (><) (-) (int '0'))
         
     let parse = Seq.head
     let solvePart1 captcha = solve captcha 2
@@ -40,7 +40,7 @@ module Day3 =
             [(-1, -1); (-1, 0); (-1, 1); (0, -1); (0, 1); (1, -1); (1, 0); (1, 1)]
             |> Seq.map (fun (x, y) -> (newX + x, newY + y))
             |> Seq.filter (Map.containsKey >< posMap)
-            |> Seq.sumBy (Map.find >< posMap) |> (List.item >< grid)
+            |> Seq.sumBy ((Map.find >< posMap) >> (List.item >< grid))
 
     let getNextPos (x, y) = 
         if (y <= 0) && (x <= -y) && (y <= x) then (x + 1, y)
@@ -51,7 +51,7 @@ module Day3 =
     let rec buildGrid grid maxDepth posMap newPos = 
         let newValue = getNextValue grid posMap newPos
         if newValue > maxDepth then newValue 
-        else buildGrid (grid @ [ newValue ]) maxDepth (posMap.Add(newPos, grid.Length)) (getNextPos newPos)
+        else buildGrid (grid @ [ newValue ]) maxDepth (Map.add newPos grid.Length posMap) (getNextPos newPos)
 
     let parse = Seq.head >> int
     let solvePart1 = manhattanDistance
@@ -79,27 +79,43 @@ module Day5 =
     let solvePart1 = solve ((+) 1)
     let solvePart2 = solve (fun x -> if x >= 3 then (x - 1) else (x + 1))
 
-let runSolver' (parse : string seq -> 'a) (solvePart1 : 'a -> 'b) (solvePart2 : 'a -> 'b) input = 
-    let processedInput = parse input
-    let output1 = solvePart1 processedInput
-    let output2 = solvePart2 processedInput
-    printfn "Part 1: %i" output1
-    printfn "Part 2: %i" output2
+module Day6 = 
+    let serialiseBanks = Array.map (fun i -> i.ToString()) >> String.concat ","
 
-let runSolver problemName input = 
+    let distribute' numBanks maxV maxI i v =
+        let doesOverlap = ((if i <= maxI then numBanks else 0) + i) <= (maxI + (maxV % numBanks))
+        (if i = maxI then 0 else v) + (maxV / numBanks) + (if doesOverlap then 1 else 0)
+
+    let rec solve seen c banks= 
+        if (Map.containsKey (serialiseBanks banks) seen) then (seen, banks)
+        else
+        let maxV = Array.max banks
+        let maxI = Array.findIndex ((=) maxV) banks
+        solve (Map.add (serialiseBanks banks) c seen) (c + 1) (Array.mapi (distribute' (Seq.length banks) maxV maxI) banks)
+
+    let parse = Seq.head >> split >> Array.map int
+    let solvePart1 = solve Map.empty 0 >> fst >> Map.count
+    let solvePart2 = solve Map.empty 0 >> (fun (seen, banks) -> (Map.count seen) - (Map.find (serialiseBanks banks) seen))
+
+let runSolver' parse solvePart1 solvePart2 input = 
+    let sw = System.Diagnostics.Stopwatch.StartNew()
+    printfn "Part 1: %A (%fms)" (solvePart1 (parse input)) sw.Elapsed.TotalMilliseconds
+    sw.Restart()
+    printfn "Part 2: %A (%fms)" (solvePart2 (parse input)) sw.Elapsed.TotalMilliseconds
+
+let runSolver problemName = 
     match problemName with
-        | "Day1" -> runSolver' Day1.parse Day1.solvePart1 Day1.solvePart2 input
-        | "Day2" -> runSolver' Day2.parse Day2.solvePart1 Day2.solvePart2 input
-        | "Day3" -> runSolver' Day3.parse Day3.solvePart1 Day3.solvePart2 input
-        | "Day4" -> runSolver' Day4.parse Day4.solvePart1 Day4.solvePart2 input
-        | "Day5" -> runSolver' Day5.parse Day5.solvePart1 Day5.solvePart2 input
-        | _ -> ()
+        | "Day1" -> runSolver' Day1.parse Day1.solvePart1 Day1.solvePart2
+        | "Day2" -> runSolver' Day2.parse Day2.solvePart1 Day2.solvePart2
+        | "Day3" -> runSolver' Day3.parse Day3.solvePart1 Day3.solvePart2
+        | "Day4" -> runSolver' Day4.parse Day4.solvePart1 Day4.solvePart2
+        | "Day5" -> runSolver' Day5.parse Day5.solvePart1 Day5.solvePart2
+        | "Day6" -> runSolver' Day6.parse Day6.solvePart1 Day6.solvePart2
+        | _ -> (fun _ -> printfn "Invalid Problem: %s" problemName)
 
 [<EntryPoint>]
 let main argv = 
-    let sw = System.Diagnostics.Stopwatch.StartNew()
     let run = argv.[0] |> runSolver
     let result = argv.[1] |> File.ReadLines |> run
-    printfn "Time to run: %f" sw.Elapsed.TotalMilliseconds
     Console.ReadKey() |> ignore
     0
