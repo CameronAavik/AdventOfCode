b8083e64313e7c401c9f32b70231a33efd8a3f9f
Cameron Aavik <cameronaavik@gmail.com>
2017-12-08 17:03:19 +1000

    Moved back to modules, types felt too OOPy

--- Program.fs
+++ Program.fs
@@ -5,35 +5,28 @@ open System.IO
 let split (str : string) = str.Split()
 let (><) f a b = f b a
 
-type Day<'T> = 
-    abstract member processInput : string seq -> 'T
-    abstract member solvePart1 : 'T -> int
-    abstract member solvePart2 : 'T -> int
-
-type Day1() =
+module Day1 =
     let solve captcha windowSize = 
         Seq.append captcha captcha
         |> Seq.windowed windowSize
         |> Seq.take (Seq.length captcha)
         |> Seq.filter (fun w -> Seq.head w = Seq.last w)
         |> Seq.sumBy (Seq.head >> int >> (-) (int '0'))
+        
+    let parse = Seq.head
+    let solvePart1 captcha = solve captcha 2
+    let solvePart2 captcha = solve captcha ((Seq.length captcha / 2) + 1)
 
-    interface Day<string> with
-        member this.processInput(seq) = Seq.head seq
-        member this.solvePart1(captcha) = solve captcha 2
-        member this.solvePart2(captcha) = solve captcha ((Seq.length captcha / 2) + 1)
-
-type Day2() = 
+module Day2 = 
     let getLargestDiff ints = (Seq.max ints) - (Seq.min ints)
     let isValidDivisor ints i = (Seq.map ((*) i) ints).Intersect(ints).Any()
     let getDivisor ints = [2 .. (Seq.max ints)] |> Seq.find (isValidDivisor ints)
 
-    interface Day<int seq seq> with
-        member this.processInput(seq) = Seq.map (split >> Seq.map int) seq
-        member this.solvePart1(spreadsheet) = Seq.sumBy getLargestDiff spreadsheet
-        member this.solvePart2(spreadsheet) = Seq.sumBy getDivisor spreadsheet
+    let parse = Seq.map (split >> Seq.map int)
+    let solvePart1 = Seq.sumBy getLargestDiff
+    let solvePart2 = Seq.sumBy getDivisor
 
-type Day3() = 
+module Day3 = 
     let manhattanDistance target = 
         let ringNumber = target |> float |> sqrt |> ceil |> int |> ((><) (/) 2)
         let ringEnd = ringNumber * 2 |> (pown >< 2) |> (+) 1
@@ -60,22 +53,20 @@ type Day3() =
         if newValue > maxDepth then newValue 
         else buildGrid (grid @ [ newValue ]) maxDepth (posMap.Add(newPos, grid.Length)) (getNextPos newPos)
 
-    interface Day<int> with
-        member this.processInput(seq) = seq |> Seq.head |> int
-        member this.solvePart1(target) = manhattanDistance target
-        member this.solvePart2(target) = buildGrid List.empty target Map.empty (0, 0)
+    let parse = Seq.head >> int
+    let solvePart1 = manhattanDistance
+    let solvePart2 target = buildGrid List.empty target Map.empty (0, 0)
 
-type Day4() = 
+module Day4 = 
     let isUnique sequence = (sequence |> Seq.distinct |> Seq.length) = (sequence |> Seq.length)
     let sortedString (str : string) = str |> Seq.sort |> String.Concat
     let solve mapper = Seq.map mapper >> Seq.filter isUnique >> Seq.length
 
-    interface Day<string[] seq> with
-        member this.processInput(seq) = seq |> Seq.map split
-        member this.solvePart1(passphrases) = solve id passphrases
-        member this.solvePart2(passphrases) = solve (Seq.map sortedString) passphrases
+    let parse = Seq.map split
+    let solvePart1 = solve id
+    let solvePart2 = solve (Seq.map sortedString)
 
-type Day5() = 
+module Day5 = 
     let rec solve' currentPosition modifyOffset maze total = 
         if currentPosition < 0 || currentPosition >= Array.length maze then total
         else
@@ -84,25 +75,24 @@ type Day5() =
             solve' (currentOffset + currentPosition) modifyOffset maze (total + 1)
     let solve modifyOffset offsets = solve' 0 modifyOffset offsets 0
 
-    interface Day<int[]> with
-        member this.processInput(seq) = seq |> Seq.map int |> Seq.toArray
-        member this.solvePart1(offsets) = solve ((+) 1) offsets
-        member this.solvePart2(offsets) = solve (fun x -> if x >= 3 then (x - 1) else (x + 1)) offsets
+    let parse = Seq.map int >> Seq.toArray
+    let solvePart1 = solve ((+) 1)
+    let solvePart2 = solve (fun x -> if x >= 3 then (x - 1) else (x + 1))
 
-let runSolver' (solver : Day<'T>) input = 
-    let processedInput = solver.processInput(input)
-    let output1 = solver.solvePart1(processedInput)
-    let output2 = solver.solvePart2(processedInput)
+let runSolver' (parse : string seq -> 'a) (solvePart1 : 'a -> 'b) (solvePart2 : 'a -> 'b) input = 
+    let processedInput = parse input
+    let output1 = solvePart1 processedInput
+    let output2 = solvePart2 processedInput
     printfn "Part 1: %i" output1
     printfn "Part 2: %i" output2
 
 let runSolver problemName input = 
     match problemName with
-        | "Day1" -> runSolver' (new Day1()) input
-        | "Day2" -> runSolver' (new Day2()) input
-        | "Day3" -> runSolver' (new Day3()) input
-        | "Day4" -> runSolver' (new Day4()) input
-        | "Day5" -> runSolver' (new Day5()) input
+        | "Day1" -> runSolver' Day1.parse Day1.solvePart1 Day1.solvePart2 input
+        | "Day2" -> runSolver' Day2.parse Day2.solvePart1 Day2.solvePart2 input
+        | "Day3" -> runSolver' Day3.parse Day3.solvePart1 Day3.solvePart2 input
+        | "Day4" -> runSolver' Day4.parse Day4.solvePart1 Day4.solvePart2 input
+        | "Day5" -> runSolver' Day5.parse Day5.solvePart1 Day5.solvePart2 input
         | _ -> ()
 
 [<EntryPoint>]
