c2be4b11c175873c2ec46d600f579069faf8f89f
Cameron Aavik <cameronaavik@gmail.com>
2017-12-11 19:34:10 +1000

    refactored day 10 a bit

--- Program.fs
+++ Program.fs
@@ -171,7 +171,6 @@ module Day10 =
     let repeat n folder init = (Seq.fold (fun s i -> folder s) init [0..(n-1)])
 
     type State = { l: int array; skip: int; start: int}
-    let initState n = {l=[|0..(n-1)|];skip=0;start=0}
     let revN n s = Array.append (Array.take n s |> Array.rev) (Array.skip n s)
     let shift n s = Array.append (Array.skip n s) (Array.take n s)
     let step listLen state length = 
@@ -179,15 +178,13 @@ module Day10 =
         {state with l = state.l |> revN length |> shift shiftLen; skip = state.skip + 1; start = (state.start - shiftLen) %! listLen}
     
     let solveRound listLen init = Array.fold (step listLen) init
-    let sparseHash listLen n = (fun t -> solveRound listLen >< t) >> repeat n >< (initState listLen) >> (fun s -> shift s.start s.l)
+    let sparseHash listLen n lengths = 
+        repeat n (solveRound listLen >< lengths) {l=[|0..(listLen-1)|]; skip=0; start=0} |> (fun s -> shift s.start s.l)
     let denseHash = Array.chunkBySize 16 >> Array.map (Array.fold (^^^) 0)
     let toHexStr = Array.fold (fun h i -> h + sprintf "%02x" i) ""
     
-    let parsePart1 = splitOn ',' >> Array.map int
-    let parsePart2 = Seq.map int >> Seq.toArray >> Array.append >< [|17;31;73;47;23|]
-
-    let solvePart1 = parsePart1 >> sparseHash 256 1 >> (fun s -> s.[0] * s.[1])
-    let solvePart2 = parsePart2 >> sparseHash 256 64 >> denseHash >> toHexStr
+    let solvePart1 = splitOn ',' >> Array.map int >> sparseHash 256 1 >> (fun s -> s.[0] * s.[1])
+    let solvePart2 = Seq.map int >> Seq.toArray >> Array.append >< [|17;31;73;47;23|] >> sparseHash 256 64 >> denseHash >> toHexStr
     let solver = { parse = Seq.head; solvePart1 = solvePart1; solvePart2 = solvePart2;}
 
 module Day11 = 
