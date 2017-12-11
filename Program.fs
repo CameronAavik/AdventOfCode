71ab17331ac30ce64312795be8e857295537274d
Cameron Aavik <cameronaavik@gmail.com>
2017-12-11 20:13:10 +1000

    switched from records to tuples

--- Program.fs
+++ Program.fs
@@ -188,14 +188,11 @@ module Day10 =
     let solver = { parse = Seq.head; solvePart1 = solvePart1; solvePart2 = solvePart2;}
 
 module Day11 = 
-    type Coordinate = {x: int; y: int}
-    let dist {x=x;y=y} = (abs(x) + abs(y)) / 2
-    let getDir = function
-        | "n"  -> (0,  2) | "s"  -> (0,  -2) | "ne" -> (1, 1) | "nw" -> (-1, 1)
-        | "se" -> (1, -1) | "sw" -> (-1, -1) | _    -> (0, 0)
-    let addDir {x=x1;y=y1} (x2,y2) = {x=x1+x2;y=y1+y2}
+    let dist (x, y) = (abs(x) + abs(y)) / 2
+    let getDir = function | "n" -> (0, 2) | "s" -> (0, -2) | "ne" -> (1, 1) | "nw" -> (-1, 1) | "se" -> (1, -1) | "sw" -> (-1, -1) | _  -> (0, 0)
+    let addDir (x1,y1) (x2,y2) = (x1+x2,y1+y2)
     let step (coords, maxDist) = getDir >> addDir coords >> (fun c -> (c, max maxDist (dist c)))
-    let solve = Array.fold step ({x=0;y=0}, 0)
+    let solve = Array.fold step ((0, 0), 0)
     let solver = { parse = Seq.head >> splitOn ','; solvePart1 = solve >> fst >> dist; solvePart2 = solve >> snd}
 
 let runSolver' day input = 
