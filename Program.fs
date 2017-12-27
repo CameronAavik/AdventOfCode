1a2709a48d0d2d2d24ead67c86c8f6a76d230dfa
Cameron Aavik <cameronaavik@gmail.com>
2017-12-27 23:29:54 +1000

    some minor reformatting/renaming

--- Program.fs
+++ Program.fs
@@ -382,20 +382,24 @@ module Day20 =
 
 module Day21 =
     let maxIndex p = Array2D.length1 p - 1
-    let flatten grid = seq {for x in [0..maxIndex grid] do for y in [0..maxIndex grid] do yield grid.[x, y]}
+    let flatten grid = seq {for x in [0..maxIndex grid] do
+                                for y in [0..maxIndex grid] do 
+                                    yield grid.[x, y]}
     let asRule = splitBy " => " (Array.map (splitBy "/" array2D) >> (fun arr -> (arr.[0], arr.[1])))
     let gridToStr = flatten >> Seq.fold (fun str x -> str + x.ToString()) ""
     let genPerms (pattern, out) =
-        let f (p : 'a [,]) = Array2D.mapi (fun x y _ -> p.[x, maxIndex p - y]) p // flips
-        let r (p : 'a [,]) = Array2D.mapi (fun x y _ -> p.[maxIndex p - y, x]) p // rotates
-        let rec gen p = seq { yield p; yield (f p); yield! gen (r p)}
+        let flip (p : 'a [,]) = Array2D.mapi (fun x y _ -> p.[x, maxIndex p - y]) p
+        let rotate (p : 'a [,]) = Array2D.mapi (fun x y _ -> p.[maxIndex p - y, x]) p
+        let rec gen p = seq { yield p; yield (flip p); yield! gen (rotate p)}
         gen pattern |> Seq.take 8 |> Seq.map (fun p -> (gridToStr p, out))
     
     let iterate grid (rules : Map<string, char[,]>) =
         let currentGridSize = maxIndex grid + 1
         let subSize = if (currentGridSize % 2) = 0 then 2 else 3
         let size = currentGridSize / subSize
-        let subgrids = seq {for x in [0..size-1] do for y in [0..size-1] do yield grid.[subSize*x .. subSize*(x+1)-1, subSize*y .. subSize*(y+1)-1]}
+        let subgrids = seq {for x in [0..size-1] do 
+                                for y in [0..size-1] do 
+                                    yield grid.[subSize*x .. subSize*(x+1)-1, subSize*y .. subSize*(y+1)-1]}
         let enhancedSubgrids = subgrids |> Seq.map gridToStr |> Seq.map (Map.find >< rules) |> Seq.toArray
         let subSize' = subSize + 1
         Array2D.init (subSize' * size) (subSize' * size) (fun x y -> enhancedSubgrids.[(x/subSize' * size) + (y/subSize')].[x%subSize',y%subSize'])
