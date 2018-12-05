ae064ef807794c4ef3665088cd19f3b07ddd8b29
Cameron Aavik <cameronaavik@gmail.com>
2018-12-05 16:40:34 +1000

    fixed incorrect variable name and unneeded type

--- Program.fs
+++ Program.fs
@@ -640,15 +640,15 @@ module Year2018 =
         let solver = {parse = Seq.sort >> parseEachLine asLog; part1 = solvePart1; part2 = solvePart2}
     
     module Day5 =
-        let remainingPolymerLength =
+        let remainingPolymer =
             let processUnit polymer ch =
                 match polymer with
                 | x :: xs when abs (ch - x) = 32 -> xs
                 | xs -> ch :: xs
             Seq.fold processUnit []
-        let filterChars ch = Seq.filter (fun c -> c <> ch && (c - 32) <> ch)
-        let solvePart1 = Seq.map int >> remainingPolymerLength >> Seq.length
-        let solvePart2 (str : string) =
-            let reducedStr = str |> Seq.map int |> remainingPolymerLength
-            Seq.init 26 ((+)65 >> filterChars >< reducedStr >> remainingPolymerLength >> Seq.length) |> Seq.min
+        let filterChars str ch = Seq.filter (fun c -> c <> ch && (c - 32) <> ch) str
+        let solvePart1 = Seq.map int >> remainingPolymer >> Seq.length
+        let solvePart2 str =
+            let reducedStr = str |> Seq.map int |> remainingPolymer
+            Seq.init 26 ((+)65 >> filterChars reducedStr >> remainingPolymer >> Seq.length) |> Seq.min
         let solver = {parse = parseFirstLine asString; part1 = solvePart1; part2 = solvePart2}
