15982f4e2c775fb8984cdfdabf894b755746f2c9
Cameron Aavik <cameronaavik@gmail.com>
2018-12-12 00:30:22 +1000

    Split into a file for each day Needs to have a bit of a styling cleanup

---
+++ Challenges/2018/Day02.fs
@@ -0,0 +1,20 @@
+﻿module Year2018Day02
+
+open CameronAavik.AdventOfCode.Common
+open System
+
+let solvePart1 gifts =
+    let containsCount i = Seq.groupBy id >> Seq.exists (fun (_, k) -> Seq.length k = i)
+    let countContains i = Seq.map (containsCount i) >> Seq.filter id >> Seq.length
+    (countContains 2 gifts) * (countContains 3 gifts)
+
+let solvePart2 gifts =
+    let replaceNth str n = String.mapi (fun i x -> if i = n then '_' else x) str
+    gifts
+    |> Seq.map (fun str -> Seq.init (String.length str) (replaceNth str))
+    |> Seq.concat
+    |> Seq.groupBy id
+    |> Seq.find (fun (_, s) -> Seq.length s = 2)
+    |> (fun (s, _) -> Seq.filter ((<>)'_') s |> String.Concat)
+
+let solver = {parse = parseEachLine asString; part1 = solvePart1; part2 = solvePart2}
---
+++ Common.fs
@@ -0,0 +1,18 @@
+﻿namespace CameronAavik.AdventOfCode
+
+open System
+
+// This is a set of methods that I found myself using many times in my solutions
+module Common =
+    // Every day has a corresponding Day record which defines how to parse the file
+    // then two functions for soving each part respectively
+    type Day<'a, 'b, 'c> = { parse: string seq -> 'a; part1: 'a -> 'b; part2: 'a -> 'c }
+
+    // helper methods for parsing
+    let parseFirstLine f = Seq.head >> f
+    let parseEachLine = Seq.map
+    let asString : string -> string = id
+    let asInt : string -> int = int
+    let asStringArray : string [] -> string [] = Array.map string
+    let asIntArray : string [] -> int [] = Array.map int
+    let splitBy (c : string) f (str : string) = str.Split([| c |], StringSplitOptions.None) |> f
