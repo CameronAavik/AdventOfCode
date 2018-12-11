a3f5f2a8e853f1b26ad846a8520f1e3a3b7e9394
Cameron Aavik <cameronaavik@gmail.com>
2018-12-11 18:18:05 +1000

    Day 11 using prefix sums

--- Main.fs
+++ Main.fs
@@ -32,7 +32,7 @@ module Main =
             | 1  -> run Year2018.Day1.solver  | 2  -> run Year2018.Day2.solver  | 3  -> run Year2018.Day3.solver
             | 4  -> run Year2018.Day4.solver  | 5  -> run Year2018.Day5.solver  | 6  -> run Year2018.Day6.solver
             | 7  -> run Year2018.Day7.solver  | 8  -> run Year2018.Day8.solver  | 9  -> run Year2018.Day9.solver
-            | 10  -> run Year2018.Day10.solver
+            | 10 -> run Year2018.Day10.solver | 11 -> run Year2018.Day11.solver
             | day -> (fun _ -> printfn "Invalid Day: %i (Year %i)" day year)
         | year -> (fun _ -> printfn "Invalid Year: %i" year)
 
@@ -42,7 +42,7 @@ module Main =
         match argv.[0] with
             | "ALL" ->
                 for i in 1..25 do runDay i 2017
-                for i in 1..10 do runDay i 2018
+                for i in 1..11 do runDay i 2018
             | x -> // 2018.1 for Day 1 2018
                 let parts = x.Split('.')
                 runDay (int parts.[1]) (int parts.[0])
