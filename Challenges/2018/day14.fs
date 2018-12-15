ffb59c93369e561dfa471804784de3b280a905e5
Cameron Aavik <cameronaavik@gmail.com>
2018-12-15 13:16:32 +1000

    Day 14 now runs in under 10 seconds

--- Challenges/2018/day14.fs
+++ Challenges/2018/day14.fs
@@ -3,113 +3,109 @@
 open CameronAavik.AdventOfCode.Common
 open System
 
-// infinite zipper with a queue of items to append once it reaches the end
-type ScoreZipper = {l: int list; c: int; r: int list; scoresToAdd: int list}
-
-let shift1 {l=l; c=c; r=r; scoresToAdd=scoresToAdd} =
-    match r with
-    | x :: xs ->
-        {l=c::l; c=x; r=xs; scoresToAdd=scoresToAdd}
-    | [] -> // if we reach the end
-        match scoresToAdd with
-        | [] -> // we need to move back to the start
-            let rev = List.rev (c::l)
-            {l=[]; c=List.head rev; r=List.tail rev; scoresToAdd=[]}
-        | _ -> // add the items from the queue
-            let rev = List.rev scoresToAdd
-            {l=c::l; c=List.head rev; r=List.tail rev; scoresToAdd=[]}
-
-// does not support negative movements
-let rec move =
-    function
-    | 0 -> id
-    | n -> shift1 >> move (n - 1)
-
-let rec addNewRecipes newRecipes zipper =
-    match newRecipes with
-    | [] -> zipper
-    | r :: rs -> {zipper with scoresToAdd = r::zipper.scoresToAdd} |> addNewRecipes rs
-
-let pickNewRecipe zipper = move (zipper.c + 1) zipper
-
-type Elves = {elf1: ScoreZipper; elf2: ScoreZipper; numScores: int}
-
-let mapElves f {elf1=elf1; elf2=elf2; numScores=numScores} =
-    {elf1=f elf1; elf2=f elf2; numScores=numScores}
-
-let getNewRecipes elves =
-    let s1 = elves.elf1.c
-    let s2 = elves.elf2.c
-    let total = s1 + s2
-    let recipeList = [total % 10]
-    if total >= 10 then
-        (total / 10) :: recipeList
+// We maintain the scores as a lazy array. If we wish to add new scores to the
+// end, then we add it to scoresToAdd. Only when we query the scores past the 
+// length of scores do we append the scoresToAdd. elf1 and elf2 are indexes 
+// into scores showing what recipe they are currently using
+type Elves = {elf1: int; elf2: int; scores: int []; scoresToAdd: int list; numScores: int}
+let getElfIndex elf elves = if elf = 1 then elves.elf1 else elves.elf2
+let setElfIndex elf newIndex elves =
+    if elf = 1 then
+        {elves with elf1=newIndex}
     else
-        recipeList
+        {elves with elf2=newIndex}
+let getScore elf elves = elves.scores.[getElfIndex elf elves]
 
-let updateRecipes elves = 
-    let newRecipes = getNewRecipes elves
-    {elves with numScores=elves.numScores + (List.length newRecipes)}
-    |> mapElves (addNewRecipes newRecipes)
+// Return a list of new recipes based on where the elves currently are
+let getNewScores elves =
+    let s1 = getScore 1 elves
+    let s2 = getScore 2 elves
+    let total = s1 + s2
+    let scoreList = [total % 10]
+    if total >= 10 then
+        (total / 10) :: scoreList
+    else
+        scoreList
 
-let moveElves = mapElves pickNewRecipe
-let step = updateRecipes >> moveElves
+// Handle adding/queueing the new scores based on the current elf recipes
+let updateScores elves =
+    let newScores = getNewScores elves
+    let appendedScores = List.fold (fun scoreList x -> x :: scoreList) elves.scoresToAdd newScores
+    let newScoreCount = elves.numScores + (List.length newScores)
+    {elves with scoresToAdd=appendedScores; numScores=newScoreCount}
 
-let initState =
-    let elf1 = {l=[]; c=3; r=[7]; scoresToAdd=[]}
-    let elf2 = elf1 |> move 1
-    {elf1=elf1; elf2=elf2; numScores=2}
+// Takes the queued scores and adds them to the scores array
+let addQueuedScores elves =
+    let newScores = elves.scoresToAdd |> List.toArray |> Array.rev
+    {elves with scores = Array.append elves.scores newScores; scoresToAdd=[]}
 
+// Move an elf to their new index. If it causes the elf to go beyond the end of
+// the scores array, it will queue any scores and cycle back to start if needed
+let rec moveElf elf elves =
+    let numScores = Array.length elves.scores
+    let score = getElfIndex elf elves + getScore elf elves + 1
+    if score < numScores || elves.scoresToAdd = [] then
+        elves |> setElfIndex elf (score % numScores)
+    else
+        elves |> addQueuedScores |> moveElf elf
+
+// Updates new recipes, then moves both elves
+let step = updateScores >> moveElf 1 >> moveElf 2
+
+// Initial status of the elves at the beginning of the program
+let initElves = {elf1=0; elf2=1; scores=[| 3; 7 |]; scoresToAdd=[]; numScores=2}
+
+// Simulate steps until we hit target + 10 recipes
 let solvePart1 targetStr =
     let target = int targetStr
-    let rec getNRecipes elves =
+    let rec getNScores elves =
         if elves.numScores > target + 10 then
-            let e1 = elves.elf1
-            let e1Index = List.length e1.l
-            let e1AtTarget =
-                if e1Index < target then
-                    e1 |> move (target - e1Index)
-                else
-                    // i may have been too lazy to implement a negative movement for this
-                    // so it just cycles the entire list
-                    e1 |> move ((List.length e1.r) + (List.length e1.scoresToAdd) + 1 + target)
+            let appended = addQueuedScores elves
             let chars =
-                Seq.init 10 id
-                |> Seq.mapFold (fun elf _ -> (char (elf.c + int '0')), (move 1 elf)) e1AtTarget
-                |> fst
-            new String(chars |> Seq.toArray)
+                Array.sub appended.scores target 10
+                |> Array.map (fun c -> char (c + int '0'))
+            new String(chars)
         else
-            elves |> step |> getNRecipes
-    getNRecipes initState
+            elves |> step |> getNScores
+    getNScores initElves
 
-let rec foundTarget target recipes =
-    match target, recipes with
-    | (x :: xs), (y :: ys) when x = y -> foundTarget xs ys
-    | [], _ -> true
-    | _ -> false
+// Fixed size buffer of 6 integers (the length of the target)
+type Buffer = int * int * int * int * int * int
+let push (_, b2, b3, b4, b5, b6) ch = (b2, b3, b4, b5, b6, ch)
+let emptyBuffer = (0, 0, 0, 0, 0, 0)
 
-type TargetBuffer = int * int * int * int * int * int
-let pushChar (_, b2, b3, b4, b5, b6) ch =
-    (b2, b3, b4, b5, b6, ch)
+// Maintains state over the most recently added scores, how many scores have
+// been seen before, and whether or not we have found a match
+type ScoreBuffer = {target: Buffer; scores: Buffer; beforeCount: int; matchFound: bool}
+
+// Handles adding a new score and updating the ScoreBuffer appropriately
+let addScore scoreBuffer newScore =
+    if scoreBuffer.matchFound then
+        scoreBuffer
+    else
+        // Push the new score and compare with the target
+        let newBuffer = push scoreBuffer.scores newScore
+        let bufferIsTarget = scoreBuffer.target = newBuffer
+        {scoreBuffer with scores=newBuffer; beforeCount=scoreBuffer.beforeCount+1; matchFound=bufferIsTarget}
+
+let initScoreBuffer target initScores =
+    List.fold addScore {target=target; scores=emptyBuffer; beforeCount=(-6); matchFound=false} initScores
+
+let targetStrToBuffer = 
+    Seq.map (fun c -> int c - int '0') // Convert chars to ints/digits
+    >> Seq.fold push emptyBuffer
 
 let solvePart2 targetStr =
-    let target = targetStr |> Seq.map (fun c -> int c - int '0') |> Seq.fold pushChar (0, 0, 0, 0, 0, 0)
-    let rec findPattern elves recipeBuffer bufferCount =
-        let newRecipes = getNewRecipes elves
-        let rec applyRecipes newRecipes bufferCount recipes =
-            match newRecipes with
-            | [] -> (recipes, bufferCount, false)
-            | x :: xs ->
-                let newBuffer = pushChar recipes x
-                if newBuffer = target then
-                    (newBuffer, bufferCount + 1, true)
-                else
-                    applyRecipes xs (bufferCount + 1) newBuffer
-        let appliedRecipes, newBufferCount, solnFound = applyRecipes newRecipes bufferCount recipeBuffer
-        if solnFound then
-            newBufferCount
+    let rec findPattern elves scoreBuffer =
+        let newScoreBuffer =
+            getNewScores elves // Get the scores that will be added
+            |> List.fold addScore scoreBuffer // Add the new scores to the buffer
+        if newScoreBuffer.matchFound then
+            newScoreBuffer.beforeCount
         else
-            findPattern (step elves) appliedRecipes newBufferCount
-    findPattern initState (0, 0, 0, 0, 3, 7) (-4)
+            findPattern (step elves) newScoreBuffer
+    let target = targetStrToBuffer targetStr
+    let scoreBuffer = initScoreBuffer target [3 ; 7]
+    findPattern initElves scoreBuffer
 
 let solver = {parse = parseFirstLine asString; part1 = solvePart1; part2 = solvePart2}
