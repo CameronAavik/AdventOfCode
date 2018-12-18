73335b6f3426e9f44ba51ed73e2e65942c3aaf58
Cameron Aavik <cameronaavik@gmail.com>
2018-12-18 22:47:38 +1000

    Made Day 18 faster and more concise. TIL that you can use Array2D as a key in a Map.

--- Challenges/2018/Day18.fs
+++ Challenges/2018/Day18.fs
@@ -1,77 +1,61 @@
 ﻿module Year2018Day18
 
 open CameronAavik.AdventOfCode.Common
-open System
 
 type Acre = Open | Trees | Lumberyard
+let charToAcre = 
+    function
+    | '.' -> Open
+    | '|' -> Trees
+    | '#' -> Lumberyard
+    | c -> failwithf "Invalid Char %c" c
 
-let parseAcres lines =
-    let charToAcre = function | '.' -> Open | '|' -> Trees | '#' -> Lumberyard | c -> failwithf "Invalid Char %c" c
-    lines
-    |> array2D
-    |> Array2D.map charToAcre
+let parseAcres = array2D >> Array2D.map charToAcre
 
 let neighbours x y =
-    [(x - 1, y - 1); (x, y - 1); (x + 1, y - 1); (x - 1, y); (x + 1, y); (x - 1, y + 1); (x, y + 1); (x + 1, y + 1)]
+    [|(x - 1, y - 1); (x, y - 1); (x + 1, y - 1); (x - 1, y); 
+      (x + 1, y); (x - 1, y + 1); (x, y + 1); (x + 1, y + 1)|]
 
 type NeighbourCounts = {openAcres: int; treeAcres: int; lumberyards: int}
 let zeroCounts = {openAcres=0; treeAcres=0; lumberyards=0}
-let addNeighbour counts neighbour = 
-    match neighbour with
+let addNeighbourToCounts counts = 
+    function
     | Open -> {counts with openAcres=counts.openAcres + 1}
     | Trees -> {counts with treeAcres=counts.treeAcres + 1}
     | Lumberyard -> {counts with lumberyards=counts.lumberyards + 1}
 
+let getNextCellState cur {treeAcres=trees; lumberyards=lumberyards} =
+    match cur with
+    | Open -> if trees >= 3 then Trees else Open
+    | Trees -> if lumberyards >= 3 then Lumberyard else Trees
+    | Lumberyard -> if lumberyards = 0 || trees = 0 then Open else Lumberyard
+
 let step grid =
     let width = Array2D.length1 grid
     let height = Array2D.length2 grid
+    let inBounds (x, y) = 0 <= x && x < width && 0 <= y && y < height
     let getNextState x y cur =
-        let inBounds (x, y) = 0 <= x && x < width && 0 <= y && y < height
-        let neighbourCounts =
-            neighbours x y 
-            |> List.filter inBounds 
-            |> List.map (fun (x, y) -> grid.[x, y])
-            |> List.fold addNeighbour zeroCounts
-        match cur with
-        | Open -> if neighbourCounts.treeAcres >= 3 then Trees else Open
-        | Trees -> if neighbourCounts.lumberyards >= 3 then Lumberyard else Trees
-        | Lumberyard -> if neighbourCounts.lumberyards = 0 || neighbourCounts.treeAcres = 0 then Open else Lumberyard
+        neighbours x y 
+        |> Array.filter inBounds 
+        |> Array.map (fun (x, y) -> grid.[x, y])
+        |> Array.fold addNeighbourToCounts zeroCounts
+        |> getNextCellState cur
     Array2D.mapi getNextState grid
 
-let array2DToSeq arr =
-    seq { for r = 0 to Array2D.length1 arr - 1 do
-            for c = 0 to Array2D.length2 arr - 1 do
-              yield arr.[r, c] }
-
 let score (grid : Acre [,]) =
-    let counts = grid |> array2DToSeq |> Seq.fold addNeighbour zeroCounts
+    let counts = grid |> Seq.cast<Acre> |> Seq.fold addNeighbourToCounts zeroCounts
     counts.lumberyards * counts.treeAcres
 
-let solvePart1 grid =
-    Seq.init 10 id
-    |> Seq.fold (fun g _ -> step g) grid
-    |> score
-
-let serialiseGrid =
-    array2DToSeq
-    >> Seq.map (function | Open -> '.' | Trees -> '|' | Lumberyard -> '#')
-    >> String.Concat
-
+let stepN n grid = Seq.init n id |> Seq.fold (fun g _ -> step g) grid
+let solvePart1 = stepN 10 >> score
 let solvePart2 grid =
-    let target = 1000000000
     let rec stepCached i grid cache =
-        let serialised = serialiseGrid grid
-        match Map.tryFind serialised cache with
+        match Map.tryFind grid cache with
         | Some x ->
             let cycleLength = i - x
-            let stepsToTarget = (target - x) % cycleLength
-            Seq.init stepsToTarget id 
-            |> Seq.fold (fun g _ -> step g) grid
-            |> score
-        | None ->
-            let cache' = Map.add serialised i cache
-            let grid' = step grid
-            stepCached (i + 1) grid' cache'
+            let stepsToTarget = (1000000000 - x) % cycleLength
+            grid |> stepN stepsToTarget |> score
+        | None -> stepCached (i + 1) (step grid) (Map.add grid i cache)
     stepCached 0 grid Map.empty
 
 let solver = {parse = parseAcres; part1 = solvePart1; part2 = solvePart2}
