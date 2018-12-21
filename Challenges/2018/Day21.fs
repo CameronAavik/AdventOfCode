b35c4248228610fedc390ebd74e25ee3e7aaab7f
Cameron Aavik <cameronaavik@gmail.com>
2018-12-21 19:25:07 +1000

    Day 21

---
+++ Challenges/2018/Day21.fs
@@ -0,0 +1,31 @@
+﻿module Year2018Day21
+
+open CameronAavik.AdventOfCode.Common
+
+let getSeed =
+    // after comparing many inputs, the only lines that was different was line 8 
+    Seq.item 8 >> splitBy " " (fun p -> int p.[1])
+
+let rec getNextTerminatingValue innerVal acc =
+    let innerVal =
+        acc &&& 0xFF
+        |> (+) innerVal
+        |> (&&&) 0xFFFFFF
+        |> (*) 65899
+        |> (&&&) 0xFFFFFF
+    if acc < 256 then
+        innerVal
+    else
+        getNextTerminatingValue innerVal (acc >>> 8)
+
+let solvePart1 seed = getNextTerminatingValue seed 0x10000
+let solvePart2 seed =
+    let rec findLastUnique prev seen =
+        let next = getNextTerminatingValue seed (prev ||| 0x10000)
+        if Set.contains next seen then
+            prev
+        else
+            findLastUnique next (Set.add next seen)
+    findLastUnique 0 Set.empty
+
+let solver = {parse = getSeed; part1 = solvePart1; part2 = solvePart2}
