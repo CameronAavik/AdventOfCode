8f26c02eb72942c991ca61c9a91b529f27f45654
CameronAavik <CameronAavik@users.noreply.github.com>
2019-11-21 00:20:38 -0800

    Finished 2016, 2016 is really really slow, need to work on that

--- Challenges/2016/Day25.fs
+++ Challenges/2016/Day25.fs
@@ -2,12 +2,15 @@
 
 open CameronAavik.AdventOfCode.Common
 
-let parse (filename : string) =
-    filename
+let parse = parseEachLine asString >> Seq.toArray
 
-let solvePart1 lines =
-    ()
-let solvePart2 lines = 
-    ()
+let solvePart1 (instrs : string []) =
+    let val1 = (instrs.[1].Split " ").[1] |> int
+    let val2 = (instrs.[2].Split " ").[1] |> int
+    let minVal = val1 * val2
+    let rec findVal val' =
+        if val' > minVal then val' - minVal
+        else findVal ((val' <<< 2) + 2)
+    findVal 2
 
-let solver = { parse = parse; part1 = solvePart1; part2 = solvePart2 }
+let solver = { parse = parse; part1 = solvePart1; part2 = (fun _ -> "Advent of Code Finished!") }
