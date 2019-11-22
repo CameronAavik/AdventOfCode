0712802cbf0e8577badd2b8d55f385213489016b
CameronAavik <CameronAavik@users.noreply.github.com>
2019-11-21 22:51:13 -0800

    2015 Days 1 - 5

--- Challenges/2015/Day01.fs
+++ Challenges/2015/Day01.fs
@@ -2,13 +2,22 @@
 
 open CameronAavik.AdventOfCode.Common
 
-let parse = parseEachLine asInt
-// let parse fileName = ()
+let parse = parseFirstLine asString
 
 let solvePart1 input =
     input
+    |> Seq.fold (fun i c -> 
+        if c = '(' then i + 1
+        else i - 1) 0
 
 let solvePart2 input =
     input
+    |> Seq.indexed
+    |> Seq.mapFold (fun i (idx, c) -> 
+        if c = '(' then (idx + 1, i + 1), i + 1
+        else (idx + 1, i - 1), i - 1) 0
+    |> fst
+    |> Seq.find (fun (i, l) -> l < 0)
+    |> fst
 
 let solver = { parse = parse; part1 = solvePart1; part2 = solvePart2 }
