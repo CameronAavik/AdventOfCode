f2469672a572cc9d50dfee4b606dd66c2207fad8
CameronAavik <CameronAavik@users.noreply.github.com>
2019-12-01 16:50:55 -0800

    Day 22 - 25

--- Challenges/2015/Day22.fs
+++ Challenges/2015/Day22.fs
@@ -2,28 +2,94 @@
 
 open CameronAavik.AdventOfCode.Common
 
-type Entity = { HP : int; Damage : int; Armor : int; Mana : int; IsBoss : bool }
-type Spell = { ManaCost : int; InstantDamage : int; Heal : int; ArmorBoost : int; ExtraBossDamage : int; ManaRegen : int; Duration : int }
-let newSpell cost duration = { ManaCost = cost; Duration = duration; InstantDamage = 0; Heal = 0; ArmorBoost = 0; ExtraBossDamage = 0; ManaRegen = 0 }
+type Spell = MagicMissile | Drain | Shield | Poison | Recharge
+let spells = set [| MagicMissile; Drain; Shield; Poison; Recharge |]
 
-let spells = [
-    { newSpell 53 0 with InstantDamage = 4 }
-    { newSpell 73 0 with InstantDamage = 2; Heal = 2 }
-    { newSpell 113 6 with ArmorBoost = 7 }
-    { newSpell 173 3 with ExtraBossDamage = 3 }
-    { newSpell 229 5 with ManaRegen = 101 }
-]
+let spellCost = function | MagicMissile -> 53 | Drain -> 73 | Shield -> 113 | Poison -> 173  | Recharge -> 229
+let spellDuration = function | MagicMissile -> 0| Drain -> 0 | Shield -> 6 | Poison -> 6 | Recharge -> 5
 
-let solvePart1 input =
-    input
+type GameData = { BossDamage : int; PlayerTurnDamage : int }
+type State = { BossHP : int; PlayerHP : int; PlayerMana : int; Spells : Map<Spell, int> }
 
-let solvePart2 input =
-    input
+let addSpell state spell =
+    let state' =
+        match spell with
+        | MagicMissile -> { state with State.BossHP = max 0 (state.BossHP - 4) }
+        | Drain -> { state with BossHP = max 0 (state.BossHP - 2); PlayerHP = state.PlayerHP + 2 }
+        | _ -> { state with Spells = Map.add spell (spellDuration spell) state.Spells }
+    { state' with PlayerMana = state'.PlayerMana - (spellCost spell); }
 
+let applyPlayerTurnDamage gameData state =
+    { state with PlayerHP = state.PlayerHP - gameData.PlayerTurnDamage }
+
+let applyPoisonIfExists state =
+    if Map.containsKey Poison state.Spells then { state with BossHP = state.BossHP - 3 }
+    else state
+
+let applyRechargeIfExists state =
+    if Map.containsKey Recharge state.Spells then { state with PlayerMana = state.PlayerMana + 101 }
+    else state
+
+let decreaseAllSpellDurations state =
+    let newSpells = Map.fold (fun m s c -> if c > 1 then Map.add s (c - 1) m else m) Map.empty state.Spells
+    { state with Spells = newSpells }
+
+let makePlayerTurn gameData state =
+    state
+    |> applyPlayerTurnDamage gameData
+    |> applyPoisonIfExists
+    |> applyRechargeIfExists
+    |> decreaseAllSpellDurations
+
+let applyBossDamage gameData state =
+    if state.BossHP > 0 then
+        let shield = if Map.containsKey Shield state.Spells then 7 else 0
+        { state with PlayerHP = state.PlayerHP - (gameData.BossDamage - shield) }
+    else state
+
+let makeBossTurn gameData state =
+    state
+    |> applyPoisonIfExists
+    |> applyRechargeIfExists
+    |> applyBossDamage gameData
+    |> decreaseAllSpellDurations
+
+let tryAllPossibleSpells gameData state =
+    let state' = makePlayerTurn gameData state
+    spells
+    |> Set.filter (fun spell -> not (Map.containsKey spell state'.Spells)) // spell must not be active
+    |> Seq.map (fun spell -> addSpell state' spell, spellCost spell)       // use spell and capture the mana cost
+    |> Seq.filter (fun (p, _) -> p.PlayerMana > 0)                         // player's mana should be above 0
+    |> Seq.map (fun (s, c) -> makeBossTurn gameData s, c)                  // make the boss's turn
+    |> Seq.filter (fun (p, _) -> p.PlayerHP > 0)                           // make sure the player still has HP after the boss's turn
+
+let dijkstra start isFinished gameData =
+    let rec dijkstra' fringe =
+        if Set.isEmpty fringe then None
+        else
+            let (dist, vertex) as minElem = Set.minElement fringe
+            let fringe' = Set.remove minElem fringe
+            if isFinished vertex then Some dist
+            else
+                tryAllPossibleSpells gameData vertex
+                |> Seq.map (fun (v, d) -> (dist + d, v))
+                |> Set.ofSeq
+                |> Set.union fringe'
+                |> dijkstra'
+    dijkstra' (set [0, start])
+
+type Input = { BossHP : int; BossDamage : int }
 let parseBoss (lines : (int []) seq) =
     let lineArr = lines |> Seq.toArray
-    { HP = lineArr.[0].[0]; Damage = lineArr.[1].[0]; Armor = 0; Mana = 0; IsBoss = true }
+    { BossHP = lineArr.[0].[0]; BossDamage = lineArr.[1].[0] }
 
 let parse = parseEachLine extractInts >> parseBoss
 
-let solver = { parse = parse; part1 = solvePart1; part2 = solvePart2 }
+let solve playerTurnDamage (input : Input) =
+    let gameData = { BossDamage = input.BossDamage; PlayerTurnDamage = playerTurnDamage }
+    let state = { BossHP = input.BossHP; PlayerHP = 50; PlayerMana = 500; Spells = Map.empty }
+    match dijkstra state (fun s -> s.BossHP <= 0) gameData with
+    | Some dist -> dist
+    | None -> failwith "Unable to beat the boss"
+
+let solver = { parse = parse; part1 = solve 0; part2 = solve 1 }
--- Challenges/2015/Day25.fs
+++ Challenges/2015/Day25.fs
@@ -2,13 +2,19 @@
 
 open CameronAavik.AdventOfCode.Common
 
-let parse = parseEachLine asInt
-// let parse fileName = ()
+let parse = parseFirstLine extractInts
 
-let solvePart1 input =
-    input
+let getCode row column =
+    let n = row + column - 1
+    let diagEnd = n * (n + 1) / 2
+    let repetitions = diagEnd - row
+    let rec genNext c n =
+        if n = 0 then c
+        else genNext ((c * 252533L) % 33554393L) (n - 1)
+    genNext 20151125L repetitions
 
-let solvePart2 input =
-    input
+let solvePart1 (input : int []) =
+    let row, column = input.[0], input.[1]
+    getCode row column
 
 let solver = { parse = parse; part1 = solvePart1; part2 = (fun _ -> "Advent of Code Finished!") }
