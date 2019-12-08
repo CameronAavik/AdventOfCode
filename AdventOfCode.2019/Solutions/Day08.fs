63522238859649d0f8e61c1cf2e13980893864c7
CameronAavik <CameronAavik@users.noreply.github.com>
2019-12-08 15:31:46 -0800

    Refactored 2019 Day 8 a bit

--- AdventOfCode.2019/Solutions/Day08.fs
+++ AdventOfCode.2019/Solutions/Day08.fs
@@ -5,28 +5,22 @@ open CameronAavik.AdventOfCode.Common
 let parse = parseFirstLine (Seq.map (fun c -> int c - int '0')) >> Seq.toArray
 
 let solvePart1 input =
-    let getAnswer (counts : Map<int, int>)= counts.[1] * counts.[2]
+    let counts =
+        input
+        |> Array.chunkBySize (25 * 6)
+        |> Array.map (Array.countBy id >> Map.ofArray)
+        |> Array.minBy (Map.find 0)
 
-    input
-    |> Array.chunkBySize (25 * 6)
-    |> Array.map (Array.countBy id >> Map.ofArray)
-    |> Array.minBy (Map.find 0)
-    |> getAnswer
+    counts.[1] * counts.[2]
     
 let solvePart2 input = 
-    let combineLayers layer1 layer2 =
-        Array.zip layer1 layer2
-        |> Array.map (fun (p1, p2) -> if p1 = 2 then p2 else p1)
-
-    let transparentLayer = Array.create (25 * 6) 2
-
     let output =
         input
         |> Array.chunkBySize (25 * 6)
-        |> Array.fold combineLayers transparentLayer
+        |> Array.reduce (Array.map2 (fun p1 p2 -> if p1 = 2 then p2 else p1))
         |> Array.map (fun i -> if i = 1 then '#' else ' ')
         |> Array.chunkBySize 25
-        |> Array.map charsToStr
+        |> Array.map System.String.Concat
         |> String.concat "\n"
 
     // just so it's nicer to read for the runner
