78ae13cc5f88a95c09040a4462ce79e1ef38e37a
CameronAavik <CameronAavik@users.noreply.github.com>
2019-12-15 20:36:23 -0800

    Refactor 2019 Day 15 a little

--- AdventOfCode.2019/Solutions/Day15.fs
+++ AdventOfCode.2019/Solutions/Day15.fs
@@ -18,14 +18,27 @@ type Robot =
       IsOxygen : bool
       Program : ProgramState }
 
+    static member create intcode = { Pos = (0, 0); Program = Computer.create intcode |> run; IsOxygen = false }
+
 let move (x, y) dir =
     if dir = 1 then (x, y - 1)
     elif dir = 2 then (x, y + 1)
     elif dir = 3 then (x - 1, y)
     else (x + 1, y)
 
-// I will clean this up later...
-let bfs initRobot stopCondition =
+let tryMoveRobotInDir r dir =
+    match r.Program with
+    | Input f ->
+        match f (int64 dir) with
+        | Output ([response], p) ->
+            if response = 0L then None
+            else Some { Pos = move r.Pos dir; IsOxygen = response = 2L; Program = p }
+        | _ -> failwith "Program should have produced a single output"
+    | _ -> failwith "Program should have been expecting input"
+
+let getEdges r = [|1 .. 4|] |> Array.choose (tryMoveRobotInDir r)
+
+let bfs stopCondition initRobot =
     let rec bfs' level seen steps =
         let level = level |> Array.filter (fun r -> Set.contains r.Pos seen |> not)
         if Array.isEmpty level then None, steps - 1
@@ -34,30 +47,21 @@ let bfs initRobot stopCondition =
             | Some r -> Some r, steps
             | None ->
                 let newSeen = Set.union seen (level |> Array.map (fun r -> r.Pos) |> Set.ofArray)
-                let nextLevel =
-                    level
-                    |> Array.collect (fun r ->
-                        [|1 .. 4|]
-                        |> Array.choose (fun dir ->
-                            match r.Program with
-                            | Input f ->
-                                match f (int64 dir) with
-                                | Output ([response], p) ->
-                                    if response = 0L then None
-                                    else Some { Pos = move r.Pos dir; IsOxygen = response = 2L; Program = p }
-                                | p -> failwith "Program should have produced a single output"
-                            | _ -> failwith "Program should have been expecting input"
-                        ))
+                let nextLevel = level |> Array.collect getEdges
                 bfs' nextLevel newSeen (steps + 1)
     bfs' [| initRobot |] Set.empty 0
 
-let solvePart1 intcode =
-    let firstRobot = { Pos = (0, 0); Program = Computer.create intcode |> run; IsOxygen = false }
-    bfs firstRobot (fun r -> r.IsOxygen) |> snd
+let solvePart1 =
+    Robot.create
+    >> bfs (fun r -> r.IsOxygen)
+    >> snd
 
-let solvePart2 intcode =
-    let firstRobot = { Pos = (0, 0); Program = Computer.create intcode |> run; IsOxygen = false }
-    let destRobot, _ = bfs firstRobot (fun r -> r.IsOxygen)
-    bfs (Option.get destRobot) (fun _ -> false) |> snd
+let solvePart2 =
+    Robot.create
+    >> bfs (fun r -> r.IsOxygen)
+    >> fst
+    >> Option.get
+    >> bfs (fun _ -> false)
+    >> snd
 
 let solver = { parse = parseIntCode; part1 = solvePart1; part2 = solvePart2 }
