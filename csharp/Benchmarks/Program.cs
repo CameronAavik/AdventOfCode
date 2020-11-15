7b8e0339f2dc7676b711e61ee22260c4d56d3c53
CameronAavik <CameronAavik@users.noreply.github.com>
2020-11-15 12:59:13 +1000

    Use BenchmarkDotNet nightly

--- csharp/Benchmarks/Program.cs
+++ csharp/Benchmarks/Program.cs
@@ -28,17 +28,16 @@ namespace AdventOfCode.CSharp.Benchmarks
 
         public static IEnumerable<Problem> Problems()
         {
-            yield return new Problem(2016, 1);
-            //for (int year = 2015; year <= 2019; year++)
-            //{
-            //    for (int day = 1; day <= 25; day++)
-            //    {
-            //        if (GetSolverType(year, day) != null)
-            //        {
-            //            yield return new Problem(year, day);
-            //        }
-            //    }
-            //}
+            for (int year = 2015; year <= 2019; year++)
+            {
+                for (int day = 1; day <= 25; day++)
+                {
+                    if (GetSolverType(year, day) != null)
+                    {
+                        yield return new Problem(year, day);
+                    }
+                }
+            }
         }
 
         [GlobalSetup]
