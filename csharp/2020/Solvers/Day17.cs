d3e5ce74bb25438564895b5b993e59cb38d8e2f9
CameronAavik <CameronAavik@users.noreply.github.com>
2020-12-18 06:54:11 +1000

    Super speedy 2020.17

--- csharp/2020/Solvers/Day17.cs
+++ csharp/2020/Solvers/Day17.cs
@@ -7,15 +7,15 @@ namespace AdventOfCode.CSharp.Y2020.Solvers
 {
     public class Day17 : ISolver
     {
-        const int X = 1 << 0;
-        const int Y = 1 << 5;
-        const int Z = 1 << 10;
-        const int W = 1 << 15;
+        const int X = 1 << 0; // 5 bits for X
+        const int Y = 1 << 5; // 5 bits for Y
+        const int Z = 1 << 10; // 4 bits for Z
+        const int W = 1 << 14; // 4 bits for W
 
         public Solution Solve(ReadOnlySpan<char> input)
         {
-            const int originX = 8;
-            const int originY = 8;
+            const int originX = 6;
+            const int originY = 6;
 
             int width = input.IndexOf('\n');
             int height = input.Length / (width + 1);
@@ -40,7 +40,7 @@ namespace AdventOfCode.CSharp.Y2020.Solvers
 
         private static int SolvePart1(List<int> inputActiveCubes, int width, int height)
         {
-            const int originZ = 8;
+            const int originZ = 6;
 
             int activeCubeCount = 0;
             int[] activeCubes = new int[(width + 12) * (height + 12) * 13];
@@ -50,31 +50,35 @@ namespace AdventOfCode.CSharp.Y2020.Solvers
             }
 
             int[] nextActiveCubes = new int[activeCubes.Length];
-            var neighbourTotals = new Dictionary<int, int>();
+            var counter = new Day17NeighbourCounter(activeCubes.Length, (originZ + 7) * Z);
             for (int iteration = 0; iteration < 6; iteration++)
             {
-                neighbourTotals.Clear();
-                for (int i = 0; i < activeCubeCount; i++)
+                counter.ResetNeighbours();
+
+                Span<int> activeCubesSpan = activeCubes.AsSpan().Slice(0, activeCubeCount);
+                foreach (int cube in activeCubesSpan)
                 {
-                    UpdateNeighbourTotalsXYZ(activeCubes[i], neighbourTotals);
+                    counter.UpdateNeighbourTotalsXYZ(cube);
                 }
 
                 int nextActiveCubesCount = 0;
-                for (int i = 0; i < activeCubeCount; i++)
+                foreach (int cube in activeCubesSpan)
                 {
-                    int cube = activeCubes[i];
-                    if (neighbourTotals.Remove(cube, out int neighbours) && neighbours is 3 or 4)
+                    if (counter.GetNeighbourTotal(cube) is 3 or 4)
                     {
                         nextActiveCubes[nextActiveCubesCount++] = cube;
+                        counter.ResetNeighbourTotal(cube); // reset neighbour total for the cube so it doesn't get triggered by the next loop
                     }
                 }
 
-                foreach ((int inactiveCube, int neighbours) in neighbourTotals)
+                foreach (int cube in counter.Neighbours)
                 {
-                    if (neighbours == 3)
+                    if (counter.GetNeighbourTotal(cube) == 3)
                     {
-                        nextActiveCubes[nextActiveCubesCount++] = inactiveCube;
+                        nextActiveCubes[nextActiveCubesCount++] = cube;
                     }
+
+                    counter.ResetNeighbourTotal(cube);
                 }
 
                 activeCubes = nextActiveCubes;
@@ -86,8 +90,8 @@ namespace AdventOfCode.CSharp.Y2020.Solvers
 
         private static int SolvePart2(List<int> inputActiveCubes, int width, int height)
         {
-            const int originZ = 8;
-            const int originW = 8;
+            const int originZ = 6;
+            const int originW = 6;
 
             int activeCubeCount = 0;
             int[] activeCubes = new int[(width + 12) * (height + 12) * 13 * 13];
@@ -97,31 +101,35 @@ namespace AdventOfCode.CSharp.Y2020.Solvers
             }
 
             int[] nextActiveCubes = new int[activeCubes.Length];
-            var neighbourTotals = new Dictionary<int, int>();
+            var counter = new Day17NeighbourCounter(activeCubes.Length, (originW + 7) * W);
             for (int iteration = 0; iteration < 6; iteration++)
             {
-                neighbourTotals.Clear();
-                for (int i = 0; i < activeCubeCount; i++)
+                counter.ResetNeighbours();
+
+                Span<int> activeCubesSpan = activeCubes.AsSpan().Slice(0, activeCubeCount);
+                foreach (int cube in activeCubesSpan)
                 {
-                    UpdateNeighbourTotalsXYZW(activeCubes[i], neighbourTotals);
+                    counter.UpdateNeighbourTotalsXYZW(cube);
                 }
 
                 int nextActiveCubesCount = 0;
-                for (int i = 0; i < activeCubeCount; i++)
+                foreach (int cube in activeCubesSpan)
                 {
-                    int cube = activeCubes[i];
-                    if (neighbourTotals.Remove(cube, out int neighbours) && neighbours is 3 or 4)
+                    if (counter.GetNeighbourTotal(cube) is 3 or 4)
                     {
                         nextActiveCubes[nextActiveCubesCount++] = cube;
+                        counter.ResetNeighbourTotal(cube); // reset neighbour total for the cube so it doesn't get triggered by the next loop
                     }
                 }
 
-                foreach ((int inactiveCube, int neighbours) in neighbourTotals)
+                foreach (int cube in counter.Neighbours)
                 {
-                    if (neighbours == 3)
+                    if (counter.GetNeighbourTotal(cube) == 3)
                     {
-                        nextActiveCubes[nextActiveCubesCount++] = inactiveCube;
+                        nextActiveCubes[nextActiveCubesCount++] = cube;
                     }
+
+                    counter.ResetNeighbourTotal(cube);
                 }
 
                 activeCubes = nextActiveCubes;
@@ -131,42 +139,80 @@ namespace AdventOfCode.CSharp.Y2020.Solvers
             return activeCubeCount;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static void UpdateNeighbourTotalsXYZW(int pos, Dictionary<int, int> neighbourTotals)
+        class Day17NeighbourCounter
         {
-            UpdateNeighbourTotalsXYZ(pos - W, neighbourTotals);
-            UpdateNeighbourTotalsXYZ(pos, neighbourTotals);
-            UpdateNeighbourTotalsXYZ(pos + W, neighbourTotals);
-        }
+            private int[] _neighbours;
+            private int _neighbourLen = 0;
+            private int[] _neighbourTotals;
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static void UpdateNeighbourTotalsXYZ(int pos, Dictionary<int, int> neighbourTotals)
-        {
-            UpdateNeighbourTotalsXY(pos - Z, neighbourTotals);
-            UpdateNeighbourTotalsXY(pos, neighbourTotals);
-            UpdateNeighbourTotalsXY(pos + Z, neighbourTotals);
-        }
+            public Day17NeighbourCounter(int maxNeighboursLen, int maxNeighbourValue)
+            {
+                _neighbours = new int[maxNeighboursLen];
+                _neighbourTotals = new int[maxNeighbourValue + 1];
+            }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static void UpdateNeighbourTotalsXY(int pos, Dictionary<int, int> neighbourTotals)
-        {
-            UpdateNeighbourTotalsX(pos - Y, neighbourTotals);
-            UpdateNeighbourTotalsX(pos, neighbourTotals);
-            UpdateNeighbourTotalsX(pos + Y, neighbourTotals);
-        }
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void ResetNeighbours()
+            {
+                _neighbourLen = 0;
+            }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static void UpdateNeighbourTotalsX(int pos, Dictionary<int, int> neighbourTotals)
-        {
-            UpdateNeighbourTotal(pos - X, neighbourTotals);
-            UpdateNeighbourTotal(pos, neighbourTotals);
-            UpdateNeighbourTotal(pos + X, neighbourTotals);
-        }
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void UpdateNeighbourTotalsXYZW(int pos)
+            {
+                UpdateNeighbourTotalsXYZ(pos - W);
+                UpdateNeighbourTotalsXYZ(pos);
+                UpdateNeighbourTotalsXYZ(pos + W);
+            }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static void UpdateNeighbourTotal(int pos, Dictionary<int, int> neighbourTotals)
-        {
-            neighbourTotals[pos] = neighbourTotals.GetValueOrDefault(pos) + 1;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void UpdateNeighbourTotalsXYZ(int pos)
+            {
+                UpdateNeighbourTotalsXY(pos - Z);
+                UpdateNeighbourTotalsXY(pos);
+                UpdateNeighbourTotalsXY(pos + Z);
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void UpdateNeighbourTotalsXY(int pos)
+            {
+                UpdateNeighbourTotalsX(pos - Y);
+                UpdateNeighbourTotalsX(pos);
+                UpdateNeighbourTotalsX(pos + Y);
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void UpdateNeighbourTotalsX(int pos)
+            {
+                UpdateNeighbourTotal(pos - X);
+                UpdateNeighbourTotal(pos);
+                UpdateNeighbourTotal(pos + X);
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void UpdateNeighbourTotal(int pos)
+            {
+                if (_neighbourTotals[pos]++ == 0)
+                {
+                    _neighbours[_neighbourLen++] = pos;
+                }
+            }
+
+            public ReadOnlySpan<int> Neighbours
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                get
+                {
+                    return _neighbours.AsSpan().Slice(0, _neighbourLen);
+                }
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetNeighbourTotal(int pos) => _neighbourTotals[pos];
+
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void ResetNeighbourTotal(int pos) => _neighbourTotals[pos] = 0;
         }
     }
 }
