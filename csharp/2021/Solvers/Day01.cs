c324940f5b06f740aa6dbd0b58e9657a08047a6f
CameronAavik <5800980+CameronAavik@users.noreply.github.com>
2021-12-01 15:53:43 +1000

    2021 Day 1

--- csharp/2021/Solvers/Day01.cs
+++ csharp/2021/Solvers/Day01.cs
@@ -7,6 +7,25 @@ public class Day01 : ISolver
 {
     public Solution Solve(ReadOnlySpan<char> input)
     {
-        throw new NotImplementedException();
+        var spanReader = new SpanReader(input);
+
+        int part1 = 0;
+        int part2 = 0;
+        int a = -1, b = -1, c = -1;
+
+        while (!spanReader.Done)
+        {
+            int m = spanReader.ReadPosIntUntil('\n');
+
+            if (c >= 0 && m > c)
+                part1++;
+
+            if (a >= 0 && m > a)
+                part2++;
+
+            (a, b, c) = (b, c, m);
+        }
+
+        return new Solution(part1, part2);
     }
 }
--- csharp/Tests/Y2021Tests.cs
+++ csharp/Tests/Y2021Tests.cs
@@ -5,5 +5,5 @@ namespace AdventOfCode.CSharp.Tests;
 
 public class Y2021Tests
 {
-    [Fact(Skip = "Not done yet")] public void Day01() => TestHelpers.AssertDay<Day01>("", "");
+    [Fact] public void Day01() => TestHelpers.AssertDay<Day01>("1167", "1130");
 }
