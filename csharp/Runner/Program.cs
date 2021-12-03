399db69beac6804bb4a51df269ac8b9c8f3422d7
CameronAavik <5800980+CameronAavik@users.noreply.github.com>
2021-12-03 22:48:15 +1000

    Remove leaderboard code for day 3

--- csharp/Runner/Program.cs
+++ csharp/Runner/Program.cs
@@ -17,78 +17,9 @@ var parse = (string s) =>
 
 var lines = input.TrimEnd('\n').Split('\n').Select(parse).ToArray();
 
-var width = lines[0].Length;
-var oxygen = "";
-var co2 = "";
-
-var oxygens = lines;
-var co2s = lines.ToArray();
-
-for (int i = 0; i < width; i++)
-{
-    var oxygenOnes = 0;
-    var total = 0;
-    foreach (var line in lines)
-    {
-        if (line.StartsWith(oxygen))
-        {
-            if (line[i] == '1')
-                oxygenOnes++;
-            total++;
-        }
-    }
-
-    if (total == 1)
-    {
-        oxygen += oxygenOnes == 1 ? "1" : "0";
-    }
-    else if (oxygenOnes >= total - oxygenOnes)
-    {
-        oxygen += "1";
-    }
-    else
-    {
-        oxygen += "0";
-    }
-
-    var c02Ones = 0;
-    total = 0;
-    foreach (var line in lines)
-    {
-        if (line.StartsWith(co2))
-        {
-            if (line[i] == '1')
-                c02Ones++;
-            total++;
-        }
-    }
-
-    if (total == 1)
-    {
-        co2 += c02Ones == 1 ? "1" : "0";
-    }
-    else if (c02Ones * 2 == total)
-    {
-        co2 += "0";
-    }
-    else if (c02Ones < total - c02Ones)
-    {
-        co2 += "1";
-    }
-    else
-    {
-        co2 += "0";
-    }
-}
-
-var gammaVal = Convert.ToInt32(oxygen, 2);
-var epsilonVal = Convert.ToInt32(co2, 2);
-Console.WriteLine(gammaVal * epsilonVal);
-
-
 for (int i = 0; i < lines.Length; i++)
 {
     var line = lines[i];
 }
 
-Console.WriteLine(lines.Length);
+Console.WriteLine();
