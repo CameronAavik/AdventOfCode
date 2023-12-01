b648cb61457ac710041c39dc251924a27256ce05
CameronAavik <5800980+CameronAavik@users.noreply.github.com>
2023-12-01 18:34:07 +1000

    Use span pattern matching for 2023.1

--- csharp/2023/Solvers/Day01.cs
+++ csharp/2023/Solvers/Day01.cs
@@ -15,59 +15,62 @@ public class Day01 : ISolver
         int firstDigit = 0;
         int lastDigit = 0;
 
-        while (input.Length >= 1) // last character always newline
+        while (input.Length > 0) // last character always newline
         {
-            var c = input[0];
-            input = input.Slice(1);
-            switch ((char)c)
+            switch (input)
             {
-                case '\n':
+                case [(byte)'\n', ..]:
                     part1 += 10 * firstNumericalDigit + lastNumericalDigit;
                     part2 += 10 * firstDigit + lastDigit;
                     firstNumericalDigit = 0;
                     lastNumericalDigit = 0;
                     firstDigit = 0;
                     lastDigit = 0;
+                    input = input.Slice(1);
                     break;
-                case >= '1' and <= '9':
-                    lastNumericalDigit = c - '0';
+                case [>= (byte)'1' and <= (byte)'9', ..]:
+                    lastNumericalDigit = input[0] - '0';
                     lastDigit = lastNumericalDigit;
+                    input = input.Slice(1);
                     break;
-                case 'o' when input.StartsWith("ne"u8):
+                case [(byte)'o', (byte)'n', (byte)'e', ..]:
                     lastDigit = 1;
-                    input = input.Slice(1); // e could be start of eight
+                    input = input.Slice(2); // e could be start of eight
                     break;
-                case 't' when input.StartsWith("wo"u8):
+                case [(byte)'t', (byte)'w', (byte)'o', ..]:
                     lastDigit = 2;
-                    input = input.Slice(1); // o could be start of one
+                    input = input.Slice(2); // o could be start of one
                     break;
-                case 't' when input.StartsWith("hree"u8):
+                case [(byte)'t', (byte)'h', (byte)'r', (byte)'e', (byte)'e', ..]:
                     lastDigit = 3;
+                    input = input.Slice(4); // e could be start of eight
+                    break;
+                case [(byte)'f', (byte)'o', (byte)'u', (byte)'r', ..]:
+                    lastDigit = 4;
+                    input = input.Slice(4);
+                    break;
+                case [(byte)'f', (byte)'i', (byte)'v', (byte)'e', ..]:
+                    lastDigit = 5;
                     input = input.Slice(3); // e could be start of eight
                     break;
-                case 'f' when input.StartsWith("our"u8):
-                    lastDigit = 4;
+                case [(byte)'s', (byte)'i', (byte)'x', ..]:
+                    lastDigit = 6;
                     input = input.Slice(3);
                     break;
-                case 'f' when input.StartsWith("ive"u8):
-                    lastDigit = 5;
-                    input = input.Slice(2); // e could be start of eight
-                    break;
-                case 's' when input.StartsWith("ix"u8):
-                    lastDigit = 6;
-                    input = input.Slice(2);
-                    break;
-                case 's' when input.StartsWith("even"u8):
+                case [(byte)'s', (byte)'e', (byte)'v', (byte)'e', (byte)'n', ..]:
                     lastDigit = 7;
-                    input = input.Slice(3); // n could be start of nine
+                    input = input.Slice(4); // n could be start of nine
                     break;
-                case 'e' when input.StartsWith("ight"u8):
+                case [(byte)'e', (byte)'i', (byte)'g', (byte)'h', (byte)'t', ..]:
                     lastDigit = 8;
-                    input = input.Slice(3); // t could be start of two or three
+                    input = input.Slice(4); // t could be start of two or three
                     break;
-                case 'n' when input.StartsWith("ine"u8):
+                case [(byte)'n', (byte)'i', (byte)'n', (byte)'e', ..]:
                     lastDigit = 9;
-                    input = input.Slice(2); // e could be start of eight
+                    input = input.Slice(3); // e could be start of eight
+                    break;
+                default:
+                    input = input.Slice(1);
                     break;
             }
 
