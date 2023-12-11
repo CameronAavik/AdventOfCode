9a01fa611a56de240799e9989017b862b906f46d
CameronAavik <5800980+CameronAavik@users.noreply.github.com>
2023-12-11 18:00:59 +1000

    Faster Day 4

--- csharp/2023/Solvers/Day04.cs
+++ csharp/2023/Solvers/Day04.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Numerics;
-using System.Runtime.CompilerServices;
 using AdventOfCode.CSharp.Common;
 
 namespace AdventOfCode.CSharp.Y2023.Solvers;
@@ -19,10 +18,8 @@ public class Day04 : ISolver
 
         Span<ulong> winningBitSet = stackalloc ulong[2]; // enough to store a bit set for 100 numbers
         Span<ulong> numbersBitSet = stackalloc ulong[2];
-
-        int[] cardCopies = new int[numCards];
-        for (int i = 0; i < numCards; i++)
-            cardCopies[i] = 1;
+        Span<int> cardCopies = stackalloc int[10];
+        int copyIndex = 0;
 
         int part1 = 0;
         int part2 = 0;
@@ -36,10 +33,11 @@ public class Day04 : ISolver
 
             input = input.Slice(cardIdStartLen);
 
+            // The ' ' character and '0' are 0x20 and 0x30 in ascii respectively, so doing & 0xF will treat them as the same
             for (int j = 0; j < numWinningPerCard; j++)
             {
-                int num = 10 * (input[0] | 0x10) + input[1] - ('0' * 10 + '0');
-                winningBitSet[num / 64] |= 1UL << (num % 64);
+                int num = 10 * (input[0] & 0xF) + (input[1] & 0xF);
+                winningBitSet[num / 64] |= 1UL << num;
                 input = input.Slice(3);
             }
 
@@ -47,18 +45,27 @@ public class Day04 : ISolver
 
             for (int j = 0; j < numNumbersPerCard; j++)
             {
-                int num = 10 * (input[0] | 0x10) + input[1] - ('0' * 10 + '0');
-                numbersBitSet[num / 64] |= 1UL << (num % 64);
+                int num = 10 * (input[0] & 0xF) + (input[1] & 0xF);
+                numbersBitSet[num / 64] |= 1UL << num;
                 input = input.Slice(3);
             }
 
-            var matches = BitOperations.PopCount(winningBitSet[0] & numbersBitSet[0]) + BitOperations.PopCount(winningBitSet[1] & numbersBitSet[1]);
-            part1 += 1 << (matches - 1);
+            int matches = BitOperations.PopCount(winningBitSet[0] & numbersBitSet[0]) + BitOperations.PopCount(winningBitSet[1] & numbersBitSet[1]);
+            part1 += (1 << matches) >> 1; // This is equivalent to doing 2 ^ (matches - 1) except it handles the case where matches = 0 where the score should be 0
 
-            var copies = cardCopies[i];
+            int copies = cardCopies[copyIndex] + 1;
+            cardCopies[copyIndex] = 0;
             part2 += copies;
-            for (int j = i + 1; j <= Math.Min(numCards - 1, i + matches); j++)
+
+            for (int j = copyIndex + 1; j < 10 && matches > 0; j++, matches--)
                 cardCopies[j] += copies;
+
+            for (int j = 0; j < matches; j++)
+                cardCopies[j] += copies;
+
+            copyIndex++;
+            if (copyIndex == 10)
+                copyIndex = 0;
         }
 
         solution.SubmitPart1(part1);
