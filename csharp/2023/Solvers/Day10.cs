0aa2884ef1c6999ee0c7bdb610fcbd940ee99f92
CameronAavik <5800980+CameronAavik@users.noreply.github.com>
2023-12-18 20:33:27 +1000

    Faster Day 10

--- csharp/2023/Solvers/Day10.cs
+++ csharp/2023/Solvers/Day10.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using AdventOfCode.CSharp.Common;
 
 namespace AdventOfCode.CSharp.Y2023.Solvers;
@@ -10,210 +11,77 @@ public class Day10 : ISolver
 
     public static void Solve(ReadOnlySpan<byte> input, Solution solution)
     {
-        // 'paths' stores a bit for every position that is on the path
-        uint[] paths = new uint[(input.Length - 1) / 32 + 1];
-
-        // 'crossings' stores a bit for every position that acts as a boundary between if something is enclosed inside the loop or not
-        // All '|' are crossings, and every vertical S-bend, 'F---J' or 'L---7', counts as one crossing
-        uint[] crossings = new uint[(input.Length - 1) / 32 + 1];
-
-        int part1 = SolvePart1(input, crossings, paths);
-        solution.SubmitPart1(part1);
-
-        int part2 = SolvePart2(paths, crossings);
-        solution.SubmitPart2(part2);
-    }
-
-    /// <summary>
-    /// Solves part 1, while also outputting a bitset of all the crossing and the path positions.
-    /// </summary>
-    private static int SolvePart1(ReadOnlySpan<byte> input, uint[] crossings, uint[] paths)
-    {
+        ref byte inputRef = ref MemoryMarshal.GetReference(input);
         int rowLen = input.IndexOf((byte)'\n') + 1;
-        int startPos = input.IndexOf((byte)'S');
+        int startPosIndex = input.IndexOf((byte)'S');
 
-        byte startPipe = GetStartPipeTypeAndDirection(input, rowLen, startPos, out Dir dir);
+        ref byte startPos = ref Unsafe.Add(ref inputRef, (nint)startPosIndex);
+        ref byte pos = ref startPos;
+
+        // assume that S = (0, 0)
+        int x = 0;
+        int y = 0;
+
+        Dir dir;
+        if (Unsafe.Subtract(ref startPos, 1) is (byte)'L' or (byte)'F' or (byte)'-')
+            dir = Dir.West;
+        else if (Unsafe.Add(ref startPos, 1) is (byte)'J' or (byte)'7' or (byte)'-')
+            dir = Dir.East;
+        else
+            dir = Dir.South;
 
         int steps = 0;
-        int pos = startPos;
-
-        byte lastHorizontalTurn = startPipe;
-
-        // If we start on a '-' or '|', we need to set the start position onto the previous turn
-        // This is because our loop that follows the path only stops on turns
-        // In addition, we also need to find the last horizontal turn in case we are on a vertical S-bend
-        if (startPipe == '-')
-        {
-            // Starting on '-', so find the last turn
-            while (true)
-            {
-                paths[startPos / 32] |= 1U << startPos;
-                byte c = input[++startPos];
-                steps++;
-                if (c != '-')
-                {
-                    lastHorizontalTurn = c;
-                    break;
-                }
-            }
-        }
-        else if (startPipe == '|')
-        {
-            // Starting on '|', so find the last turn
-            while (true)
-            {
-                paths[startPos / 32] |= 1U << startPos;
-                crossings[startPos / 32] |= 1U << startPos;
-                startPos -= rowLen;
-                byte c = input[startPos];
-                steps++;
-
-                if (c != '|')
-                    break;
-            }
-        }
-        else
-        {
-            paths[startPos / 32] |= 1U << startPos;
-        }
+        int area = 0;
 
         while (true)
         {
             byte c;
+            int count = 1;
             switch (dir)
             {
                 case Dir.East:
-                    while ((c = input[++pos]) == '-')
-                    {
-                        paths[pos / 32] |= 1U << pos;
-                        steps++;
-                    }
+                    while ((c = pos = ref Unsafe.Add(ref pos, 1)) == '-')
+                        count++;
 
-                    paths[pos / 32] |= 1U << pos;
-                    steps++;
-
-                    if (c == 'J')
-                    {
-                        if (lastHorizontalTurn == 'F')
-                            crossings[pos / 32] |= 1U << pos;
-                        dir = Dir.North;
-                    }
-                    else if (c == '7')
-                    {
-                        if (lastHorizontalTurn == 'L')
-                            crossings[pos / 32] |= 1U << pos;
-                        dir = Dir.South;
-                    }
-
+                    steps += count;
+                    x += count;
+                    area -= count * y;
+                    dir = c == 'J' ? Dir.North : Dir.South;
                     break;
                 case Dir.West:
-                    while ((c = input[--pos]) == '-')
-                    {
-                        paths[pos / 32] |= 1U << pos;
-                        steps++;
-                    }
+                    while ((c = pos = ref Unsafe.Subtract(ref pos, 1)) == '-')
+                        count++;
 
-                    paths[pos / 32] |= 1U << pos;
-                    steps++;
-
-                    if (c == 'L')
-                    {
-                        if (lastHorizontalTurn == '7')
-                            crossings[pos / 32] |= 1U << pos;
-                        dir = Dir.North;
-                    }
-                    else if (c == 'F')
-                    {
-                        if (lastHorizontalTurn == 'J')
-                            crossings[pos / 32] |= 1U << pos;
-                        dir = Dir.South;
-                    }
-
+                    steps += count;
+                    x -= count;
+                    area += count * y;
+                    dir = c == 'L' ? Dir.North : Dir.South;
                     break;
                 case Dir.North:
-                    while ((c = input[pos -= rowLen]) == '|')
-                    {
-                        paths[pos / 32] |= 1U << pos;
-                        crossings[pos / 32] |= 1U << pos;
-                        steps++;
-                    }
+                    while ((c = pos = ref Unsafe.Subtract(ref pos, rowLen)) == '|')
+                        count++;
 
-                    paths[pos / 32] |= 1U << pos;
-                    lastHorizontalTurn = c;
-                    steps++;
+                    steps += count;
+                    y -= count;
+                    area -= count * x;
                     dir = c == '7' ? Dir.West : Dir.East;
                     break;
                 case Dir.South:
-                    while ((c = input[pos += rowLen]) == '|')
-                    {
-                        paths[pos / 32] |= 1U << pos;
-                        crossings[pos / 32] |= 1U << pos;
-                        steps++;
-                    }
+                    while ((c = pos = ref Unsafe.Add(ref pos, rowLen)) == '|')
+                        count++;
 
-                    paths[pos / 32] |= 1U << pos;
-                    lastHorizontalTurn = c;
-                    steps++;
+                    steps += count;
+                    y += count;
+                    area += count * x;
                     dir = c == 'L' ? Dir.East : Dir.West;
                     break;
             }
 
-            if (pos == startPos)
+            if (Unsafe.AreSame(ref pos, ref startPos))
                 break;
         }
 
-        return steps / 2;
-    }
-
-    private static byte GetStartPipeTypeAndDirection(ReadOnlySpan<byte> input, int rowLen, int startPos, out Dir dir)
-    {
-        bool leftIncoming = input[startPos - 1] is (byte)'L' or (byte)'F' or (byte)'-';
-        bool rightIncoming = input[startPos + 1] is (byte)'J' or (byte)'7' or (byte)'-';
-        bool upIncoming = input[startPos - rowLen] is (byte)'F' or (byte)'7' or (byte)'|';
-        bool downIncoming = input[startPos + rowLen] is (byte)'F' or (byte)'7' or (byte)'|';
-
-        if (leftIncoming)
-        {
-            dir = Dir.West;
-            if (upIncoming)
-                return (byte)'J';
-            else if (downIncoming)
-                return (byte)'7';
-            else
-                return (byte)'-';
-        }
-        else if (rightIncoming)
-        {
-            dir = Dir.East;
-            return (byte)(upIncoming ? 'L' : 'F');
-        }
-        else
-        {
-            dir = Dir.South;
-            return (byte)'|';
-        }
-    }
-
-    private static int SolvePart2(uint[] paths, uint[] crossings)
-    {
-        int part2 = 0;
-        uint initialMask = 0U;
-        for (int i = 0; i < paths.Length; i++)
-        {
-            // see https://lemire.me/blog/2018/02/21/iterating-over-set-bits-quickly/ for an explanation of how this works
-            uint crossing = crossings[i];
-            uint insideMask = initialMask;
-            while (crossing != 0)
-            {
-                uint lsb = crossing & (~crossing + 1);
-                insideMask ^= ~(lsb - 1); // flips all the bits after the lsb
-                crossing ^= lsb;
-                initialMask = ~initialMask;
-            }
-
-            part2 += BitOperations.PopCount(~paths[i] & insideMask);
-        }
-
-        return part2;
+        solution.SubmitPart1(steps / 2);
+        solution.SubmitPart2((area - steps) / 2 + 1);
     }
 }
