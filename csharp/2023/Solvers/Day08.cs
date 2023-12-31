3ff2885019f119a9c6649c1d0b3c6bb9e524688d
Cameron Aavik <5800980+CameronAavik@users.noreply.github.com>
2024-01-01 09:12:26 +1000

    Multi input benchmarking (#17)
    * Multi-input benchmarking
    
    * Undo temp changes
    
    * Some more cleanup

--- csharp/2023/Solvers/Day08.cs
+++ csharp/2023/Solvers/Day08.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using AdventOfCode.CSharp.Common;
 
 namespace AdventOfCode.CSharp.Y2023.Solvers;
--- csharp/Benchmarks/Y2023Solver.cs
+++ csharp/Benchmarks/Y2023Solver.cs
@@ -1,5 +1,5 @@
-﻿using AdventOfCode.CSharp.Common;
-using AdventOfCode.CSharp.Runner;
+﻿using System.IO;
+using AdventOfCode.CSharp.Common;
 using AdventOfCode.CSharp.Y2023.Solvers;
 using BenchmarkDotNet.Attributes;
 
@@ -30,7 +30,7 @@ namespace AdventOfCode.CSharp.Benchmarks;
 [GenericTypeArguments(typeof(Day23))]
 [GenericTypeArguments(typeof(Day24))]
 [GenericTypeArguments(typeof(Day25))]
-public class Y2023Solver<TSolver> : SolverBenchmarkBase<TSolver> where TSolver : ISolver
+public class Y2023Solver<TSolver> : MultiInputSolverBenchmarkBase<TSolver> where TSolver : ISolver
 {
 }
 
@@ -39,41 +39,50 @@ public class AllDays2023
 {
     private readonly char[] _part1Buffer = new char[128];
     private readonly char[] _part2Buffer = new char[128];
-    private readonly byte[][] _inputs = new byte[25][];
+    private readonly byte[][][] _inputs = new byte[25][][];
 
     public AllDays2023()
     {
         for (int i = 0; i < 25; i++)
-            _inputs[i] = AdventRunner.GetInputAsync(2023, i + 1).GetAwaiter().GetResult();
+        {
+            _inputs[i] = new byte[5][];
+            var inputFolder = $"input/2023/extra/day{i + 1:D2}";
+            int j = 0;
+            foreach (var file in Directory.EnumerateFiles(inputFolder))
+                _inputs[i][j++] = File.ReadAllBytes(file);
+        }
     }
 
-    [Benchmark]
+    [Benchmark(OperationsPerInvoke = 5)]
     public void SolveAllDays()
     {
-        Day01.Solve(_inputs[0], new(_part1Buffer, _part2Buffer));
-        Day02.Solve(_inputs[1], new(_part1Buffer, _part2Buffer));
-        Day03.Solve(_inputs[2], new(_part1Buffer, _part2Buffer));
-        Day04.Solve(_inputs[3], new(_part1Buffer, _part2Buffer));
-        Day05.Solve(_inputs[4], new(_part1Buffer, _part2Buffer));
-        Day06.Solve(_inputs[5], new(_part1Buffer, _part2Buffer));
-        Day07.Solve(_inputs[6], new(_part1Buffer, _part2Buffer));
-        Day08.Solve(_inputs[7], new(_part1Buffer, _part2Buffer));
-        Day09.Solve(_inputs[8], new(_part1Buffer, _part2Buffer));
-        Day10.Solve(_inputs[9], new(_part1Buffer, _part2Buffer));
-        Day11.Solve(_inputs[10], new(_part1Buffer, _part2Buffer));
-        Day12.Solve(_inputs[11], new(_part1Buffer, _part2Buffer));
-        Day13.Solve(_inputs[12], new(_part1Buffer, _part2Buffer));
-        Day14.Solve(_inputs[13], new(_part1Buffer, _part2Buffer));
-        Day15.Solve(_inputs[14], new(_part1Buffer, _part2Buffer));
-        Day16.Solve(_inputs[15], new(_part1Buffer, _part2Buffer));
-        Day17.Solve(_inputs[16], new(_part1Buffer, _part2Buffer));
-        Day18.Solve(_inputs[17], new(_part1Buffer, _part2Buffer));
-        Day19.Solve(_inputs[18], new(_part1Buffer, _part2Buffer));
-        Day20.Solve(_inputs[19], new(_part1Buffer, _part2Buffer));
-        Day21.Solve(_inputs[20], new(_part1Buffer, _part2Buffer));
-        Day22.Solve(_inputs[21], new(_part1Buffer, _part2Buffer));
-        Day23.Solve(_inputs[22], new(_part1Buffer, _part2Buffer));
-        Day24.Solve(_inputs[23], new(_part1Buffer, _part2Buffer));
-        Day25.Solve(_inputs[24], new(_part1Buffer, _part2Buffer));
+        for (int i = 0; i < 5; i++)
+        {
+            Day01.Solve(_inputs[0][i], new(_part1Buffer, _part2Buffer));
+            Day02.Solve(_inputs[1][i], new(_part1Buffer, _part2Buffer));
+            Day03.Solve(_inputs[2][i], new(_part1Buffer, _part2Buffer));
+            Day04.Solve(_inputs[3][i], new(_part1Buffer, _part2Buffer));
+            Day05.Solve(_inputs[4][i], new(_part1Buffer, _part2Buffer));
+            Day06.Solve(_inputs[5][i], new(_part1Buffer, _part2Buffer));
+            Day07.Solve(_inputs[6][i], new(_part1Buffer, _part2Buffer));
+            Day08.Solve(_inputs[7][i], new(_part1Buffer, _part2Buffer));
+            Day09.Solve(_inputs[8][i], new(_part1Buffer, _part2Buffer));
+            Day10.Solve(_inputs[9][i], new(_part1Buffer, _part2Buffer));
+            Day11.Solve(_inputs[10][i], new(_part1Buffer, _part2Buffer));
+            Day12.Solve(_inputs[11][i], new(_part1Buffer, _part2Buffer));
+            Day13.Solve(_inputs[12][i], new(_part1Buffer, _part2Buffer));
+            Day14.Solve(_inputs[13][i], new(_part1Buffer, _part2Buffer));
+            Day15.Solve(_inputs[14][i], new(_part1Buffer, _part2Buffer));
+            Day16.Solve(_inputs[15][i], new(_part1Buffer, _part2Buffer));
+            Day17.Solve(_inputs[16][i], new(_part1Buffer, _part2Buffer));
+            Day18.Solve(_inputs[17][i], new(_part1Buffer, _part2Buffer));
+            Day19.Solve(_inputs[18][i], new(_part1Buffer, _part2Buffer));
+            Day20.Solve(_inputs[19][i], new(_part1Buffer, _part2Buffer));
+            Day21.Solve(_inputs[20][i], new(_part1Buffer, _part2Buffer));
+            Day22.Solve(_inputs[21][i], new(_part1Buffer, _part2Buffer));
+            Day23.Solve(_inputs[22][i], new(_part1Buffer, _part2Buffer));
+            Day24.Solve(_inputs[23][i], new(_part1Buffer, _part2Buffer));
+            Day25.Solve(_inputs[24][i], new(_part1Buffer, _part2Buffer));
+        }
     }
 }
