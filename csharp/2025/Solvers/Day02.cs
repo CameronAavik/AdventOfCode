1ded336bcfa5bd5063e7024d42212d3b00a72542
CameronAavik <5800980+CameronAavik@users.noreply.github.com>
2025-12-02 18:18:36 +1000

    Now down to 380ns for day 2

--- csharp/2025/Solvers/Day02.cs
+++ csharp/2025/Solvers/Day02.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Globalization;
+using System.Runtime.CompilerServices;
 using AdventOfCode.CSharp.Common;
 
 namespace AdventOfCode.CSharp.Y2025.Solvers;
@@ -8,27 +8,60 @@ public class Day02 : ISolver
 {
     public static void Solve(ReadOnlySpan<byte> input, Solution solution)
     {
-        long part1 = 0;
-        long part2 = 0;
+        ulong part1 = 0;
+        ulong part2 = 0;
 
-        foreach (var range in input.TrimEnd((byte)'\n').Split((byte)','))
+        var i = 0;
+        while (i < input.Length)
         {
-            var rangeStr = input[range];
-            var dashIndex = rangeStr.IndexOf((byte)'-');
-            var start = long.Parse(rangeStr[..dashIndex], CultureInfo.InvariantCulture);
-            var end = long.Parse(rangeStr[(dashIndex + 1)..], CultureInfo.InvariantCulture);
+            var startIndex = i;
+            var start = (ulong)input[i++] - '0';
+            while (input[i++] is byte c and > (byte)'-')
+                start = start * 10 + (ulong)(c - '0');
+            var startLen = i - startIndex - 1;
 
-            var digits = 2;
-            var digitsStart = 10L;
-            var digitsEnd = 99L;
-            while (end >= digitsStart)
+            var endIndex = i;
+            var end = (ulong)input[i++] - '0';
+            while (input[i++] is byte c and > (byte)',')
+                end = end * 10 + (ulong)(c - '0');
+            var endLen = i - endIndex - 1;
+
+            for (var digits = startLen; digits <= endLen; digits++)
             {
-                if (start <= digitsEnd)
-                    AddInvalidIds(digits, Math.Max(start, digitsStart), Math.Min(end, digitsEnd), ref part1, ref part2);
-
-                digits++;
-                digitsStart *= 10;
-                digitsEnd = (digitsEnd + 1) * 10 - 1;
+                switch (digits)
+                {
+                    case 2:
+                        part1 += InvalidIdsWithPattern(start, end, 10, 11);
+                        break;
+                    case 3:
+                        part2 += InvalidIdsWithPattern(start, end, 100, 111);
+                        break;
+                    case 4:
+                        part1 += InvalidIdsWithPattern(start, end, 1000, 0101);
+                        break;
+                    case 5:
+                        part2 += InvalidIdsWithPattern(start, end, 10000, 11111);
+                        break;
+                    case 6:
+                        part1 += InvalidIdsWithPattern(start, end, 100000, 001001);
+                        part2 += InvalidIdsWithPattern(start, end, 100000, 010101);
+                        part2 -= InvalidIdsWithPattern(start, end, 100000, 111111);
+                        break;
+                    case 7:
+                        part2 += InvalidIdsWithPattern(start, end, 1000000, 1111111);
+                        break;
+                    case 8:
+                        part1 += InvalidIdsWithPattern(start, end, 10000000, 000010001);
+                        break;
+                    case 9:
+                        part2 += InvalidIdsWithPattern(start, end, 100000000, 001001001);
+                        break;
+                    case 10:
+                        part1 += InvalidIdsWithPattern(start, end, 1000000000, 0000100001);
+                        part2 += InvalidIdsWithPattern(start, end, 1000000000, 0101010101);
+                        part2 -= InvalidIdsWithPattern(start, end, 1000000000, 1111111111);
+                        break;
+                }
             }
         }
 
@@ -36,46 +69,16 @@ public class Day02 : ISolver
         solution.SubmitPart2(part1 + part2);
     }
 
-    private static void AddInvalidIds(int digits, long start, long end, ref long part1, ref long part2)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static ulong InvalidIdsWithPattern(ulong start, ulong end, ulong min, ulong pattern)
     {
-        switch (digits)
-        {
-            case 2:
-                part1 += SumInRangeWithPattern(start, end, 11);
-                break;
-            case 3:
-                part2 += SumInRangeWithPattern(start, end, 111);
-                break;
-            case 4:
-                part1 += SumInRangeWithPattern(start, end, 0101);
-                break;
-            case 5:
-                part2 += SumInRangeWithPattern(start, end, 11111);
-                break;
-            case 6:
-                part1 += SumInRangeWithPattern(start, end, 001001);
-                part2 += SumInRangeWithPattern(start, end, 010101);
-                part2 -= SumInRangeWithPattern(start, end, 111111);
-                break;
-            case 7:
-                part2 += SumInRangeWithPattern(start, end, 1111111);
-                break;
-            case 8:
-                part1 += SumInRangeWithPattern(start, end, 000010001);
-                break;
-            case 9:
-                part2 += SumInRangeWithPattern(start, end, 001001001);
-                break;
-            case 10:
-                part1 += SumInRangeWithPattern(start, end, 0000100001);
-                part2 += SumInRangeWithPattern(start, end, 0101010101);
-                part2 -= SumInRangeWithPattern(start, end, 1111111111);
-                break;
-        }
-    }
+        if (start < min)
+            start = min;
 
-    private static long SumInRangeWithPattern(long start, long end, long pattern)
-    {
+        var max = 10 * min;
+        if (end > max)
+            end = max;
+
         // Identify the first and last multiplier inside the range
         var first = (start + pattern - 1) / pattern;
         var last = end / pattern;
