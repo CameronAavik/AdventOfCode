34b2641077d6251d0b9216d91dcec555bf7b194f
CameronAavik <5800980+CameronAavik@users.noreply.github.com>
2025-12-08 07:31:02 +1000

    Vectorized day 7

--- csharp/2025/Solvers/Day07.cs
+++ csharp/2025/Solvers/Day07.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
 using AdventOfCode.CSharp.Common;
 
 namespace AdventOfCode.CSharp.Y2025.Solvers;
@@ -7,67 +11,97 @@ public class Day07 : ISolver
 {
     public static void Solve(ReadOnlySpan<byte> input, Solution solution)
     {
+        ref var inputRef = ref MemoryMarshal.GetReference(input);
+
         var startIndex = input.IndexOf((byte)'S');
         var width = input[(startIndex + 1)..].IndexOf((byte)'\n') + startIndex + 1;
 
-        var counts = new long[width];
-        counts[startIndex] = 1;
+        var counts = width > 256 ? new long[width] : stackalloc long[256];
+        counts = counts[..width];
+
+        ref var countsRef = ref MemoryMarshal.GetReference(counts);
+        Unsafe.Add(ref countsRef, startIndex) = 1;
 
         var part1 = 0;
 
         var minCol = startIndex;
         var maxCol = startIndex;
+        var tachyons = Vector256.Create((byte)'^');
         for (var rowIndex = 0; rowIndex < input.Length - 1; rowIndex += width + 1)
         {
-            var row = input[rowIndex..(rowIndex + maxCol + 1)];
+            var alignedStart = maxCol - Vector256<byte>.Count + 1;
+            var extraVectorsNeeded = (alignedStart - minCol + Vector256<byte>.Count - 1) / Vector256<byte>.Count;
+            alignedStart -= extraVectorsNeeded * Vector256<byte>.Count;
 
-            var col = minCol + 1;
-            var prevTachyon = -1;
-
-            if (row[minCol] == (byte)'^')
+            if (alignedStart < 0)
             {
-                var prev = counts[minCol];
-                if (prev > 0)
-                {
-                    part1++;
-                    counts[minCol - 1] += prev;
-                    counts[minCol] = 0;
-                    counts[minCol + 1] += prev;
-                }
+                alignedStart += Vector256<byte>.Count;
+                var unalignedStart = minCol;
+                var v = Vector256.LoadUnsafe(ref inputRef, (nuint)(rowIndex + unalignedStart));
+                var tachyonLocations = Vector256.Equals(v, tachyons).ExtractMostSignificantBits();
 
-                col = minCol + 2;
-                prevTachyon = minCol;
-                minCol--;
+                var bitsToProcess = alignedStart - unalignedStart;
+                tachyonLocations &= (1U << bitsToProcess) - 1;
+
+                while (tachyonLocations != 0)
+                {
+                    var t = tachyonLocations & (~tachyonLocations + 1);
+                    var location = unalignedStart + BitOperations.TrailingZeroCount(t);
+                    ref var prevRef = ref Unsafe.Add(ref countsRef, location);
+                    var prev = prevRef;
+                    if (prev > 0)
+                    {
+                        part1++;
+                        Unsafe.Subtract(ref prevRef, 1) += prev;
+                        Unsafe.Add(ref prevRef, 1) += prev;
+                        prevRef = 0;
+
+                        if (location == minCol)
+                            minCol--;
+                    }
+
+                    tachyonLocations ^= t;
+                }
             }
 
-            while (col < row.Length)
+            var lastCol = -1;
+            for (var col = alignedStart; col < maxCol; col += Vector256<byte>.Count)
             {
-                var nextTachyon = row[col..].IndexOf((byte)'^');
-                if (nextTachyon == -1)
-                    break;
-
-                col += nextTachyon;
-
-                var prev = counts[col];
-                if (prev > 0)
+                var v = Vector256.LoadUnsafe(ref inputRef, (nuint)(rowIndex + col));
+                var tachyonLocations = Vector256.Equals(v, tachyons).ExtractMostSignificantBits();
+                while (tachyonLocations != 0)
                 {
-                    part1++;
-                    counts[col - 1] += prev;
-                    counts[col] = 0;
-                    counts[col + 1] += prev;
-                }
+                    var t = tachyonLocations & (~tachyonLocations + 1);
+                    lastCol = col + BitOperations.TrailingZeroCount(t);
+                    ref var prevRef = ref Unsafe.Add(ref countsRef, lastCol);
+                    var prev = prevRef;
+                    if (prev > 0)
+                    {
+                        part1++;
+                        Unsafe.Subtract(ref prevRef, 1) += prev;
+                        Unsafe.Add(ref prevRef, 1) += prev;
+                        prevRef = 0;
 
-                prevTachyon = col;
-                col += 2;
+                        if (lastCol == minCol)
+                            minCol--;
+                    }
+
+                    tachyonLocations ^= t;
+                }
             }
 
-            if (prevTachyon == maxCol)
+            if (lastCol == maxCol)
                 maxCol++;
         }
 
-        long part2 = 0;
-        foreach (var count in counts)
-            part2 += count;
+        var end = (nuint)(counts.Length - (counts.Length % Vector256<long>.Count));
+        var part2Sum = Vector256<long>.Zero;
+        for (nuint i = 0; i < end; i += (nuint)Vector256<long>.Count)
+            part2Sum += Vector256.LoadUnsafe(ref countsRef, i);
+
+        var part2 = Vector256.Sum(part2Sum);
+        for (var i = end; i < (nuint)counts.Length; i++)
+            part2 += Unsafe.Add(ref countsRef, i);
 
         solution.SubmitPart1(part1);
         solution.SubmitPart2(part2);
